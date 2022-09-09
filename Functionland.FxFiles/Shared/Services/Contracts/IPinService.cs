60733e7a001d2bc41166693668bb313895209d97
Mehran Davoudi <mehran.davoudi@hotmail.com>
2022-09-09 20:09:05 +0430

    Add initial models. ab#15

---
+++ Functionland.FxFiles/Shared/Services/Contracts/IPinService.cs
@@ -0,0 +1,9 @@
+﻿namespace Functionland.FxFiles.Shared.Services.Contracts
+{
+    public interface IPinService
+    {
+        Task SetFolderPinAsync(string path, bool isPinned, CancellationToken? cancellationToken = null);
+        Task SetFilePinAsync(string path, bool isPinned, CancellationToken? cancellationToken = null);
+        Task<FsArtifact> GetPinnedArtifactsAsync();
+    }
+}
