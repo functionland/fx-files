827924d0d3473e06b7b35a184a83c86ab9269e59
Zahra Bayat <zahra.bayat13799@gmail.com>
2022-09-11 21:57:13 +0430

    Add sample test. ab#51

--- Functionland.FxFiles/Web/Pages/HomePage.razor.cs
+++ Functionland.FxFiles/Web/Pages/HomePage.razor.cs
@@ -1,6 +1,19 @@
-﻿namespace Functionland.FxFiles.App.Pages;
+﻿using System.Runtime.CompilerServices;
+
+namespace Functionland.FxFiles.App.Pages;
 
 public partial class HomePage
 {
+    public IFileService FileService { get; set; }
+
+    public async Task Test()
+    {
+        var list =  FileService.GetArtifactsAsync();
+        await foreach(var item in list)
+        {
+            Console.WriteLine("Test");
+            
+        }
+    }
 }
 
