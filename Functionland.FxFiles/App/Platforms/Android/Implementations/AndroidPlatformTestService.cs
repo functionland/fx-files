16b1b1f6ca3214734c3fcf13190bb6589a265685
hamedhajiloo <hamedhajiloo96@gmail.com>
2022-09-12 06:03:47 +0430

    Create AndroidPlatformTestService. ab#193

---
+++ Functionland.FxFiles/App/Platforms/Android/Implementations/AndroidPlatformTestService.cs
@@ -0,0 +1,19 @@
+﻿using Functionland.FxFiles.Shared.TestInfra.Implementations;
+
+namespace Functionland.FxFiles.App.Platforms.Android.Implementations
+{
+    public partial class AndroidPlatformTestService : PlatformTestService
+    {
+        [AutoInject] FakeFileServicePlatformTest FakeFileServicePlatformTest { get; set; }
+
+
+        protected override List<IPlatformTest> OnGetTests()
+        {
+            return new List<IPlatformTest>()
+            {
+                FakeFileServicePlatformTest
+            };
+        }
+
+    }
+}
