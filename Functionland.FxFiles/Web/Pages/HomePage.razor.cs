21165eeddec5da795ab54968ea17715a1ad6fd82
Zahra Bayat <zahra.bayat13799@gmail.com>
2022-09-12 13:37:28 +0430

    Add test button for testing android file service. ab#51

--- Functionland.FxFiles/Web/Pages/HomePage.razor.cs
+++ Functionland.FxFiles/Web/Pages/HomePage.razor.cs
@@ -1,12 +1,24 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace Functionland.FxFiles.App.Pages;
 
 public partial class HomePage
 {
+    [AutoInject] public IFileService FileService { get; set; }
     private void NavigateToTestExplorerComponent()
     {
         NavigationManager.NavigateTo("/TestExplorer");
+    }
+    public async Task Test()
+    {
+        var list = FileService.GetArtifactsAsync();
+        await foreach (var item in list)
+        {
+            Console.WriteLine("Test");
+
+        }
     }
 }
 
