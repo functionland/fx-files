b9961ee5c0bc8909f9f4f39ba95fc54b5f65d274
Zahra Bayat <zahra.bayat13799@gmail.com>
2022-09-13 22:44:49 +0430

    Add smaple test componet for all AndroidFileService method. ab#47#48#49#50

--- Functionland.FxFiles/Web/Pages/HomePage.razor.cs
+++ Functionland.FxFiles/Web/Pages/HomePage.razor.cs
@@ -17,47 +17,69 @@ public partial class HomePage
     {
         //  sample paths:
         var sDPath = "/storage/1EED-3A0B/Alarms";
-        var sDfilePath = "/storage/1EED-3A0B/Alarms/IMG_20220912_050004-Copy01.jpg";
+        var sDfilePath = "/storage/1EED-3A0B/Alarms/IMG_20220912_050004.jpg";
         var internalPath = "/storage/emulated/0/Pictures/TestCreateFolder";
         var internalfilePath = "/storage/emulated/0/Pictures/IMG_20220912_050004.jpg";
         var internalfilePath2 = "/storage/emulated/0/Audiobooks/IMG_20220912_050004.jpg";
-        
+        var sDfilePath2 = "/storage/1EED-3A0B/Music/IMG_20220912_050004.jpg";
+
         //############################################
         //GetArtifactsAsync
         //var list = FileService.GetArtifactsAsync();
-        var list = FileService.GetArtifactsAsync(internalPath);
+        //var list = FileService.GetArtifactsAsync(internalPath);
         //var list = FileService.GetArtifactsAsync(sDPath); 
-        var results = new List<FsArtifact>();
+        //var results = new List<FsArtifact>();
 
-        await foreach (var element in list)
-        {
-            Console.WriteLine("Test: " + element);
-            results.Add(element);
-        }
-        _ = results;
+        //await foreach (var element in list)
+        //{
+        //    Console.WriteLine("Test: " + element);
+        //    results.Add(element);
+        //}
+        //_ = results;
 
         //############################################
         //GetFileContentAsync
         //var InternalFileContentStream = await FileService.GetFileContentAsync(internalfilePath);
         //var sDFileContentStream = await FileService.GetFileContentAsync(sDfilePath);
 
-        //############################################ ((((test failed))))
+        //############################################
         //CreateFileAsync 
-        //var fileArtifact = await FileService.CreateFileAsync(internalfilePath2, InternalFileContentStream);
+        //var fileArtifact = await FileService.CreateFileAsync(sDfilePath2, sDFileContentStream);
+
+        //############################################
+        //CreateFilesAsync
 
         //############################################
         //CreateFolderAsync
         //var folderArtifact = await FileService.CreateFolderAsync(internalPath, "TestCreateFolder");
         //var folderArtifact = await FileService.CreateFolderAsync(sDPath, "TestCreateFolder");
 
-        //###########################################((((test failed))))
+        //###########################################
         //DeleteArtifactsAsync
         //await FileService.DeleteArtifactsAsync(results.ToArray());
 
         //###########################################((((test failed))))
-        //
-        var des = "/storage/emulated/0/Pictures/";
-        await FileService.CopyArtifactsAsync(results.ToArray(), des);
+        //CopyArtifactsAsync
+        //var des = "/storage/emulated/0/Pictures/";
+        //await FileService.CopyArtifactsAsync(results.ToArray(), des);
+
+        //############################################((((test failed))))
+        //MoveArtifactsAsync
+        //var des = "/storage/emulated/0/Notifications/";
+        //await FileService.MoveArtifactsAsync(results.ToArray(), des);
+
+        //############################################
+        //RenameFileAsync
+        //var filePath = "/storage/emulated/0/Pictures/IMG_20220912_050004.jpg";
+        //var filePath = "/storage/1EED-3A0B/Alarms/IMG_20220912_050004.jpg";
+        //await FileService.RenameFileAsync(filePath, "TestSucsess");
+
+        //############################################
+        //RenameFolderAsync
+        //var folderPath = "/storage/emulated/0/Pictures";
+        var folderPath = "/storage/1EED-3A0B/Alarms";
+        await FileService.RenameFolderAsync(folderPath, "FolderRenamed");
+
     }
 }
 
