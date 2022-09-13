9f30425e7148be21c3258758e997ec9703cba9cb
Zahra Bayat <zahra.bayat13799@gmail.com>
2022-09-13 18:04:51 +0430

    Reverse to initial implementation. ab#51

--- Functionland.FxFiles/App/Platforms/Android/Implementations/AndroidFileService.cs
+++ Functionland.FxFiles/App/Platforms/Android/Implementations/AndroidFileService.cs
@@ -19,114 +19,20 @@ namespace Functionland.FxFiles.App.Platforms.Android.Implementations
 {
     public partial class AndroidFileService : LocalDeviceFileService
     {
-        private static android.Net.Uri contentUri = MediaStore.Files.GetContentUri("external");
-        private async Task<List<FsArtifact>> GetDrivesAsync()
-        {
-            // ToDo: Get the right drives
-            var drives = new List<FsArtifact>
-            {
-                new FsArtifact()
-                {
-                    Name = "internal",
-                    ArtifactType = FsArtifactType.Drive,
-                    ProviderType = FsFileProviderType.InternalMemory
-                }
-            };
-
-            if (true)
-            {
-                drives.Add(
-                new FsArtifact()
-                {
-                    Name = "sdcard01",
-                    ArtifactType = FsArtifactType.Drive,
-                    ProviderType = FsFileProviderType.ExternalMemory
-                });
-            }
-
-            return drives;
-        }
-        private FsFileProviderType GetProviderTypeFromPath(string path)
-        {
-            // ToDo: How to get it from the path
-            if (path.StartsWith("intenal"))
-            {
-                return FsFileProviderType.InternalMemory;
-            }
-            else if (path.StartsWith("sdcard"))
-            {
-                return FsFileProviderType.ExternalMemory;
-            }
-            else
-                throw new Exception($"Unknown file provider for path: {path}");
-            
-        }
-
-        private async IAsyncEnumerable<FsArtifact> GetFilesAsync(Bundle queryArge)
-        {
-            string[] projection = {
-                    MediaStore.IMediaColumns.BucketId,
-                    MediaStore.IMediaColumns.MimeType,
-                    MediaStore.IMediaColumns.DisplayName,
-                    MediaStore.IMediaColumns.Size,
-                    IBaseColumns.Id,
-                    MediaStore.IMediaColumns.Data,
-                    MediaStore.IMediaColumns.DateAdded,
-                    MediaStore.IMediaColumns.DateModified
-            };
-
-            using var cursor = MauiApplication.Current.ContentResolver?.Query(
-                        contentUri,
-                        projection,
-                        queryArge,
-                        null
-                        );
-
-            if (cursor is not null && cursor.MoveToFirst())
-            {
-                do
-                {
-                    var parentId = cursor.GetInt(0);
-                    var mimeType = cursor.GetString(1);
-                    var name = cursor.GetString(2);
-                    var size = cursor.GetLong(3);
-                    var id = cursor.GetLong(4);
-                    var data = cursor.GetString(5);
-                    var dateAdded = DateTimeOffset.FromUnixTimeSeconds(cursor.GetLong(6));
-                    var dateModifiedUnixFormat = cursor.GetLong(7);
-                    DateTimeOffset dateModified = dateModifiedUnixFormat == 0 ? dateAdded : DateTimeOffset.FromUnixTimeSeconds(dateModifiedUnixFormat);
-
-                    yield return new FsArtifact
-                    {
-                        ParentId = parentId,
-                        MimeType = mimeType,
-                        Name = name,
-                        Size = size,
-                        Id = id,
-                        FullPath = data,
-                        FileExtension = Path.GetExtension(data),
-                        LastModifiedDateTime = dateModified
-                    };
-                }
-                while (cursor.MoveToNext());
-            }
-        }
-
-        
         public override Task CopyArtifactsAsync(FsArtifact[] artifacts, string destination, CancellationToken? cancellationToken = null)
         {
             return base.CopyArtifactsAsync(artifacts, destination, cancellationToken);
         }
 
         public override Task<FsArtifact> CreateFileAsync(string path, Stream stream, CancellationToken? cancellationToken = null)
-                {
+        {
             return base.CreateFileAsync(path, stream, cancellationToken);
-                }
+        }
 
         public override Task<List<FsArtifact>> CreateFilesAsync(IEnumerable<(string path, Stream stream)> files, CancellationToken? cancellationToken = null)
-                {
+        {
             return base.CreateFilesAsync(files, cancellationToken);
-                }
+        }
 
         public override Task<FsArtifact> CreateFolderAsync(string path, string folderName, CancellationToken? cancellationToken = null)
         {
@@ -138,43 +44,9 @@ namespace Functionland.FxFiles.App.Platforms.Android.Implementations
             return base.DeleteArtifactsAsync(artifacts, cancellationToken);
         }
 
-        public override async IAsyncEnumerable<FsArtifact> GetArtifactsAsync(string? path = null, string? searchText = null, CancellationToken? cancellationToken = null)
+        public override IAsyncEnumerable<FsArtifact> GetArtifactsAsync(string? path = null, string? searchText = null, CancellationToken? cancellationToken = null)
         {
-            if (path is null)
-            {
-                var drives = await GetDrivesAsync();
-                foreach (var drive in drives)
-                {
-                    yield return drive;
-                }
-                yield break;
-            }
-
-            var provider = GetProviderTypeFromPath(path);
-            if (provider == FsFileProviderType.InternalMemory)
-            {
-                // ToDo: Get from internal memory properly.
-                await foreach (var artifact in base.GetArtifactsAsync(path, searchText, cancellationToken))
-                {
-                    yield return artifact;
-                }
-            }
-            else if (provider == FsFileProviderType.ExternalMemory)
-            {
-                string selection = $@"(({MediaStore.IMediaColumns.Data} == {path}));";
-
-                if (searchText is not null)
-                {
-                    selection = selection + $@"AND ( {MediaStore.IMediaColumns.DisplayName} like '%{searchText}%' );";
-                }
-
-                Bundle bundle = new Bundle();
-                bundle.PutString(ContentResolver.QueryArgSqlSelection, selection);
-                await foreach (var artifact in GetFilesAsync(bundle))
-                {
-                    yield return artifact;
-                };
-            }
+            return base.GetArtifactsAsync(path, searchText, cancellationToken);
         }
 
         public override Task MoveArtifactsAsync(FsArtifact[] artifacts, string destination, CancellationToken? cancellationToken = null)
@@ -196,5 +68,6 @@ namespace Functionland.FxFiles.App.Platforms.Android.Implementations
         {
             throw new NotImplementedException();
         }
+
     }
 }
