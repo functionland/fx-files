ec715956d6e38cd37cc3b29a8188bded350a5267
ysmoradi <ysmoradi@outlook.com>
2022-09-14 21:30:02 +0430

    refactor-ab#28

--- Functionland.FxFiles/App/Platforms/Windows/App.xaml.cs
+++ Functionland.FxFiles/App/Platforms/Windows/App.xaml.cs
@@ -7,6 +7,12 @@ public partial class App
     public App()
     {
         InitializeComponent();
+        UnhandledException += OnUnhandledException;
+    }
+
+    private void OnUnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
+    {
+        e.Handled = true;
     }
 
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiAppBuilder().Build();
