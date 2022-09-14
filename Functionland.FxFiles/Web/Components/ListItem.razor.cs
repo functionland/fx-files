7b960bd5ac6e69ee90e1242beea9a51ce5b67733
Maryam Davoudi <Davoudi.maryam@outlook.com>
2022-09-14 11:18:04 +0430

    Set view mode for list item component ab#160

--- Functionland.FxFiles/Web/Components/ListItem.razor.cs
+++ Functionland.FxFiles/Web/Components/ListItem.razor.cs
@@ -1,6 +1,10 @@
-﻿namespace Functionland.FxFiles.App.Components
+﻿using Functionland.FxFiles.App.Components.Common;
+
+namespace Functionland.FxFiles.App.Components
 {
     public partial class ListItem
     {
+        [Parameter]
+        public ListItemConfig Config { get; set; } = new ListItemConfig(ViewMode.list, "", "", false, "");
     }
 }
