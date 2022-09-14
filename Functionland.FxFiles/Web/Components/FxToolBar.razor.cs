64041b3f114d74e666949c015634216bea6f3438
MH Rastegari <mhrwin8.1@gmail.com>
2022-09-14 20:11:49 +0430

    Add main toolbar UI ab#157

--- Functionland.FxFiles/Web/Components/FxToolBar.razor.cs
+++ Functionland.FxFiles/Web/Components/FxToolBar.razor.cs
@@ -1,6 +1,11 @@
 ﻿namespace Functionland.FxFiles.App.Components;
 
-public partial class Header
+public partial class FxToolBar
 {
+    [Parameter]
+    public string? Title { get; set; }
+
+    [Parameter, EditorRequired]
+    public bool IsMainToolBar { get; set; }
 
 }
