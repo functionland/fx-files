78ca968fc8f44e7e128e78aa72f34bb53dc6a9cc
hamedhajiloo <hamedhajiloo96@gmail.com>
2022-09-15 00:26:10 +0430

    Create WindowsPlatformTestService. ab#194

---
+++ Functionland.FxFiles/App/Platforms/Windows/Implementations/Test/WindowsFileServicePlatformTest.cs
@@ -0,0 +1,18 @@
+﻿using Functionland.FxFiles.Shared.Services;
+using Functionland.FxFiles.Shared.TestInfra.Implementations;
+
+namespace Functionland.FxFiles.App.Platforms.Windows.Implementations.Test;
+
+public partial class WindowsFileServicePlatformTest : FileServicePlatformTest
+{
+    public override string Title => "WindowsFileService Test";
+
+    public override string Description => "Tests the common features of this FileService";
+
+    protected override IFileService OnGetFileService()
+    {
+        return FakeFileServiceFactory.CreateTypical();
+    }
+
+    protected override string OnGetTestsRootPath() => "fakeroot";
+}
