46502964d542363db69765716d8688c5ec21efb4
hamedhajiloo <hamedhajiloo96@gmail.com>
2022-09-15 00:25:19 +0430

    Create AndroidPlatformTestService. ab#194

---
+++ Functionland.FxFiles/App/Platforms/Android/Implementations/Test/AndroidFileServicePlatformTest.cs
@@ -0,0 +1,18 @@
+﻿using Functionland.FxFiles.Shared.Services;
+using Functionland.FxFiles.Shared.TestInfra.Implementations;
+
+namespace Functionland.FxFiles.App.Platforms.Android.Implementations.Test;
+
+public partial class AndroidFileServicePlatformTest : FileServicePlatformTest
+{
+    public override string Title => "AndroidFileService Test";
+
+    public override string Description => "Tests the common features of this FileService";
+
+    protected override IFileService OnGetFileService()
+    {
+        return FakeFileServiceFactory.CreateTypical();
+    }
+
+    protected override string OnGetTestsRootPath() => "fakeroot";//TODO:Replace correct root path
+}
---
+++ Functionland.FxFiles/App/Platforms/Android/Implementations/Test/AndroidPlatformTestService.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Functionland.FxFiles.App.Platforms.Android.Implementations.Test;
+
+public partial class AndroidPlatformTestService : PlatformTestService
+{
+    [AutoInject] AndroidFileServicePlatformTest AndroidFileServicePlatformTest { get; set; }
+
+    protected override List<IPlatformTest> OnGetTests()
+    {
+        return new List<IPlatformTest>()
+        {
+            AndroidFileServicePlatformTest
+        };
+    }
+}
