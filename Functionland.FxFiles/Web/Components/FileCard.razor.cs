1bf0981eadf311d8d6e315bfd5e4154a6e3225a3
Your Name <aminmahjoob3172@gmail.com>
2022-09-15 20:11:28 +0430

    Add FileCard fuctionality ab#158

--- Functionland.FxFiles/Web/Components/FileCard.razor.cs
+++ Functionland.FxFiles/Web/Components/FileCard.razor.cs
@@ -2,5 +2,24 @@
 {
     public partial class FileCard
     {
+        [Parameter]
+        public bool IsFavorite { get; set; }
+
+        [Parameter]
+        public bool IsDisable { get; set; }
+
+        [Parameter, EditorRequired]
+        public string? FileName { get; set; }
+
+        [Parameter, EditorRequired]
+        public string? FileFormat { get; set; }
+
+        [Parameter, EditorRequired]
+        public string? ModifiedDate { get; set; }
+        
+        [Parameter, EditorRequired]
+        public string? FileSize { get; set; }
+        
+        public bool IsPressed { get; set; }
     }
 }
