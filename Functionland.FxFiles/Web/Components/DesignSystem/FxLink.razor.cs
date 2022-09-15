3a8f8c60673e924ea23858a1973da73fd90ad759
Your Name <aminmahjoob3172@gmail.com>
2022-09-15 22:46:32 +0430

    Add FxLink functionality ab#132

--- Functionland.FxFiles/Web/Components/DesignSystem/FxLink.razor.cs
+++ Functionland.FxFiles/Web/Components/DesignSystem/FxLink.razor.cs
@@ -2,5 +2,17 @@
 {
     public partial class FxLink
     {
+        [Parameter]
+        public bool IsLarge { get; set; }
+
+        [Parameter]
+        public bool IsLeftIcon { get; set; }
+
+        [Parameter]
+        public bool IsRightIcon { get; set; }
+
+        [Parameter]
+        public bool IsDisabled { get; set; }
+
     }
 }
