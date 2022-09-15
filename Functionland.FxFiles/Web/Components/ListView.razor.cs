430e999922061d33bd4fe8a02e37d0aefe9a3d03
Maryam Davoudi <Davoudi.maryam@outlook.com>
2022-09-16 02:03:53 +0430

    Add action bar for list ab#228

--- Functionland.FxFiles/Web/Components/ListView.razor.cs
+++ Functionland.FxFiles/Web/Components/ListView.razor.cs
@@ -1,8 +1,36 @@
-﻿namespace Functionland.FxFiles.App.Components
+﻿using Functionland.FxFiles.App.Components.Common;
+using System.ComponentModel;
+
+namespace Functionland.FxFiles.App.Components
 {
     public partial class ListView
     {
-        [Parameter]
-        public string? Title { get; set; }
+        [Parameter, EditorRequired]
+        public string ListTitle { get; set; } = String.Empty;
+
+        [Parameter, EditorRequired]
+        public List<ListItemConfig> ListItems { get; set; } = new List<ListItemConfig>() { };
+
+        public ViewModeEnum ViewMode = ViewModeEnum.list;
+        public SortOrderEnum SortOrder = SortOrderEnum.asc;
+
+
+        public void ToggleSortOrder()
+        {
+            if (SortOrder == SortOrderEnum.asc)
+            {
+                SortOrder = SortOrderEnum.desc;
+            } else
+            {
+                SortOrder = SortOrderEnum.asc;
+            }
+
+            //todo: change order of list items
+        }
+
+        public void OnSortChange()
+        {
+            //todo: Open sort bottom sheet
+        }
     }
 }
--- Functionland.FxFiles/Web/Pages/MyDevice.razor.cs
+++ Functionland.FxFiles/Web/Pages/MyDevice.razor.cs
@@ -4,14 +4,15 @@ namespace Functionland.FxFiles.App.Pages
 {
     public partial class MyDevice
     {
-        public ViewMode ViewMode = ViewMode.list;
+        public string ListTitle = "My Device";
         public List<ListItemConfig> ListItems = new List<ListItemConfig>()
         {
-            new ListItemConfig(ViewMode.list, "CsInternship", "subtext1", true, "folder"),
-            new ListItemConfig(ViewMode.list, "text.txt", "subtext2", true, "text-file"),
-            new ListItemConfig(ViewMode.list, "green.mp3", "subtext3", true, "audio-file"),
-            new ListItemConfig(ViewMode.list, "word.pdf", "subtext4", true, "pdf-file"),
-            new ListItemConfig(ViewMode.list, "jojo.jpg", "subtext5", true, "photo-file")
+            new ListItemConfig("CsInternship", "subtext1", true, "folder"),
+            new ListItemConfig("text.txt", "subtext2", true, "text-file"),
+            new ListItemConfig("green.mp3", "subtext3", true, "audio-file"),
+            new ListItemConfig("word.pdf", "subtext4", true, "pdf-file"),
+            new ListItemConfig("jojo.jpg", "subtext5", true, "photo-file")
         };
+
     }
 }
