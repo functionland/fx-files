b4466a7d5c9e7e858e1ddff124e579d7641a9cb7
Hootan Hemmati <hootanhemmati@outlook.com>
2022-09-15 20:17:58 +0430

    Modify FileDetailView ab#108

--- Functionland.FxFiles/Web/Components/FileDetailView.razor.cs
+++ Functionland.FxFiles/Web/Components/FileDetailView.razor.cs
@@ -2,5 +2,10 @@
 {
     public partial class FileDetailView
     {
+        [Parameter, EditorRequired]
+        public bool IsImageFile { get; set; } = false;
+
+        [Parameter, EditorRequired]
+        public string? FileName { get; set; }
     }
 }
