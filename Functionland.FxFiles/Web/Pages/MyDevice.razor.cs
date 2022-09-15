157ad33a11ab2010f16e7c61137c203397a9c1a2
Maryam Davoudi <Davoudi.maryam@outlook.com>
2022-09-15 09:27:34 +0430

    Add my device List style ab#228

--- Functionland.FxFiles/Web/Pages/MyDevice.razor.cs
+++ Functionland.FxFiles/Web/Pages/MyDevice.razor.cs
@@ -4,14 +4,14 @@ namespace Functionland.FxFiles.App.Pages
 {
     public partial class MyDevice
     {
-        public ViewMode ViewMode = ViewMode.grid;
+        public ViewMode ViewMode = ViewMode.list;
         public List<ListItemConfig> ListItems = new List<ListItemConfig>()
         {
-            new ListItemConfig(ViewMode.grid, "CsInternship", "subtext1", true, "folder"),
-            new ListItemConfig(ViewMode.grid, "text.txt", "subtext2", true, "text-file"),
-            new ListItemConfig(ViewMode.grid, "green.mp3", "subtext3", true, "audio-file"),
-            new ListItemConfig(ViewMode.grid, "word.pdf", "subtext4", true, "pdf-file"),
-            new ListItemConfig(ViewMode.grid, "jojo.jpg", "subtext5", true, "photo-file")
+            new ListItemConfig(ViewMode.list, "CsInternship", "subtext1", true, "folder"),
+            new ListItemConfig(ViewMode.list, "text.txt", "subtext2", true, "text-file"),
+            new ListItemConfig(ViewMode.list, "green.mp3", "subtext3", true, "audio-file"),
+            new ListItemConfig(ViewMode.list, "word.pdf", "subtext4", true, "pdf-file"),
+            new ListItemConfig(ViewMode.list, "jojo.jpg", "subtext5", true, "photo-file")
         };
     }
 }
