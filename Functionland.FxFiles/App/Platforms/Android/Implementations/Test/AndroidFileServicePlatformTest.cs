b3dd553b04302c2b151fe6079abc3a02396bbcac
Zahra Bayat <zahra.bayat13799@gmail.com>
2022-09-15 23:06:48 +0430

    Inject FileService in AndroidFileServicePlatformTest. ab#194

--- Functionland.FxFiles/App/Platforms/Android/Implementations/Test/AndroidFileServicePlatformTest.cs
+++ Functionland.FxFiles/App/Platforms/Android/Implementations/Test/AndroidFileServicePlatformTest.cs
@@ -5,14 +5,20 @@ namespace Functionland.FxFiles.App.Platforms.Android.Implementations.Test;
 
 public partial class AndroidFileServicePlatformTest : FileServicePlatformTest
 {
+    public IFileService FileService { get; set; }
+
+    public AndroidFileServicePlatformTest(IFileService fileService)
+    {
+        FileService = fileService;
+    }
     public override string Title => "AndroidFileService Test";
 
     public override string Description => "Tests the common features of this FileService";
 
     protected override IFileService OnGetFileService()
     {
-        return FakeFileServiceFactory.CreateTypical();
+        return FileService;
     }
 
-    protected override string OnGetTestsRootPath() => "fakeroot";//TODO:Replace correct root path
+    protected override string OnGetTestsRootPath() => "/storage/emulated/0/";//TODO:Replace correct root path
 }
