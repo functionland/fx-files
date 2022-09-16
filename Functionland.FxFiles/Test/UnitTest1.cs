effcb42e57fa23d9d381243633bf5ae8dc933959
Mehran Davoudi <mehran.davoudi@hotmail.com>
2022-09-17 01:05:00 +0430

    Add initial Test project. ab#194

---
+++ Functionland.FxFiles/Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Functionland.FxFiles.Shared.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
