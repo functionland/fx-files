c9a2368382c28174947f1994e69151945d8a0590
Your Name <aminmahjoob3172@gmail.com>
2022-09-16 15:54:48 +0430

    Add avatar functionality ab#136

--- Functionland.FxFiles/Web/Components/DesignSystem/FxAvatar.razor.cs
+++ Functionland.FxFiles/Web/Components/DesignSystem/FxAvatar.razor.cs
@@ -2,5 +2,26 @@
 {
     public partial class FxAvatar
     {
+        [Parameter, EditorRequired]
+        public FxAvatarSizeEnum FxAvatarSizeType { get; set; }
+
+        [Parameter]
+        public FxAvatarStatusEnum FxAvatarStatusType { get; set; }      
+    }
+    
+    public enum FxAvatarSizeEnum
+    {
+        XSmall,
+        Small,
+        Medium,
+        Large,
+        XLarge
+    }
+
+    public enum FxAvatarStatusEnum
+    {
+        Selected,
+        DeSelected,
+        Edit
     }
 }
