922f14889f07bc33af2b36c20d95dd03edd2ca37
negar <negar.ghasemi@hotmail.com>
2022-09-17 00:38:49 +0430

    Create interface of file watch service ab#151

---
+++ Functionland.FxFiles/Shared/Services/Implementations/FileWatchService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functionland.FxFiles.Shared.Services.Implementations
+{
+    public class FileWatchService : IFileWatchService
+    {
+        public Task InitialyzeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
