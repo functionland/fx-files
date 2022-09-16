9ae1f6c9d5bde8ae23a9bf6e7bc66346f7aec2ea
Maryam Davoudi <Davoudi.maryam@outlook.com>
2022-09-16 14:18:40 +0430

    Add card config ab#158

--- Functionland.FxFiles/Web/Components/FileCard.razor.cs
+++ Functionland.FxFiles/Web/Components/FileCard.razor.cs
@@ -1,25 +1,10 @@
-﻿namespace Functionland.FxFiles.App.Components
+﻿using Functionland.FxFiles.App.Components.Common;
+
+namespace Functionland.FxFiles.App.Components
 {
     public partial class FileCard
     {
         [Parameter]
-        public bool IsFavorite { get; set; }
-
-        [Parameter]
-        public bool IsDisable { get; set; }
-
-        [Parameter, EditorRequired]
-        public string? FileName { get; set; }
-
-        [Parameter, EditorRequired]
-        public string? FileFormat { get; set; }
-
-        [Parameter, EditorRequired]
-        public string? ModifiedDate { get; set; }
-        
-        [Parameter, EditorRequired]
-        public string? FileSize { get; set; }
-        
-        public bool IsPressed { get; set; }
+        public FileCardConfig CartConfig { get; set; }
     }
 }
--- Functionland.FxFiles/Web/Pages/MyDevice.razor.cs
+++ Functionland.FxFiles/Web/Pages/MyDevice.razor.cs
@@ -1,6 +1,13 @@
-﻿namespace Functionland.FxFiles.App.Pages
+﻿using Functionland.FxFiles.App.Components.Common;
+
+namespace Functionland.FxFiles.App.Pages
 {
     public partial class MyDevice
     {
+
+        public List<FileCardConfig> PinnedCards = new List<FileCardConfig>
+        {
+            new FileCardConfig(true, true, true, "Cs intenrship", ".txt", "date", "file size")
+        };
     }
 }
