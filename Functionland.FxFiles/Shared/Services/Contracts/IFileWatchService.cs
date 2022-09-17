76afc038b2ecb12a6ec6efe5d953da2cf20801fd
negar <negar.ghasemi@hotmail.com>
2022-09-17 09:40:39 +0430

    Add deligate ab#151

--- Functionland.FxFiles/Shared/Services/Contracts/IFileWatchService.cs
+++ Functionland.FxFiles/Shared/Services/Contracts/IFileWatchService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Functionland.FxFiles.Shared.TestInfra.Implementations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,8 @@ namespace Functionland.FxFiles.Shared.Services.Contracts
     public interface IFileWatchService
     {
         Task InitialyzeAsync();
+        delegate Task AsyncEventHandler<TEventArgs>(object? sender, TEventArgs e);
+        public event AsyncEventHandler<ArtifactChangeEventArgs> ArtifactChangeEvent;
     }
+
 }
