27e37c9161975194963d3d7aa5b7e4ec425572b9
MH Rastegari <mhrwin8.1@gmail.com>
2022-09-18 22:19:54 +0430

    Add neccesarry parameters to FxTextInput

--- Functionland.FxFiles/Web/Components/DesignSystem/FxTextInput.razor.cs
+++ Functionland.FxFiles/Web/Components/DesignSystem/FxTextInput.razor.cs
@@ -2,5 +2,19 @@
 {
     public partial class FxTextInput
     {
+        [Parameter]
+        public string? Text { get; set; }
+
+        [Parameter]
+        public string? Label { get; set; }
+
+        [Parameter]
+        public string? PlaceHolder { get; set; }
+
+        [Parameter]
+        public string? QuestionText { get; set; }
+
+        [Parameter]
+        public string? ErrorMessage { get; set; }
     }
 }
