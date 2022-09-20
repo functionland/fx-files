fbbc2ead70ac859a4048178ab16b4eacedbfb859
Ziba Seyfaei <Ziba.seifaie@outlook.com>
2022-09-20 14:54:17 +0430

    Add Sha256 algoritm. ab#239

---
+++ Functionland.FxFiles/Shared/Utils/MakeHashData.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Functionland.FxFiles.Shared.Utils
+{
+    public static class MakeHashData
+    {
+        public static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using SHA256 sha256Hash = SHA256.Create();
+            // ComputeHash - returns byte array  
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+            // Convert byte array to a string   
+            StringBuilder builder = new();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+}
