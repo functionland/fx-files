1961589b0d1ee7071ec073f8f1b67078c7a73149
Zahra Bayat <zahra.bayat13799@gmail.com>
2022-09-20 22:26:18 +0430

    Implement MakeThumbnailAsync. ab#238
    Co-Authored-By: siroossha <113217130+siroossha@users.noreply.github.com>

--- Functionland.FxFiles/App/Platforms/Android/Implementations/AndroidThumbnailService.cs
+++ Functionland.FxFiles/App/Platforms/Android/Implementations/AndroidThumbnailService.cs
@@ -1,20 +1,56 @@
-﻿using Functionland.FxFiles.Shared.Models;
-using Functionland.FxFiles.Shared.Utils;
-using android = Android;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Android.Graphics;
+﻿using Functionland.FxFiles.Shared.Utils;
+using Path = System.IO.Path;
+using Microsoft.Maui.Graphics.Platform;
+using System.Reflection;
+using Microsoft.Maui.Graphics;
+using IImage = Microsoft.Maui.Graphics.IImage;
 
 namespace Functionland.FxFiles.App.Platforms.Android.Implementations
 {
     public class AndroidThumbnailService : LocalThumbnailService
     {
-        public override Task<string> MakeThumbnailAsync(FsArtifact fsArtifact, CancellationToken? cancellationToken = null)
+        public override async Task<string> MakeThumbnailAsync(FsArtifact fsArtifact, CancellationToken? cancellationToken = null)
         {
-            throw new NotImplementedException();
+            var thumbPath = GetThumbnailFullPath(fsArtifact);
+
+            if (File.Exists(thumbPath)) return thumbPath;
+
+            IImage image;
+            using var streamReader = new StreamReader(fsArtifact.FullPath);
+
+            using var stream = streamReader.BaseStream;
+            image = PlatformImage.FromStream(stream);
+
+            if (image != null)
+            {
+                IImage newImage = image.Downsize(150, true);
+                using MemoryStream memStream = new MemoryStream();
+                using var fileStream = new FileStream(thumbPath, FileMode.Create);
+                newImage.Save(memStream);
+                memStream.Seek(0, SeekOrigin.Begin);
+                await memStream.CopyToAsync(fileStream); 
+            }
+
+            return thumbPath;
+        }
+
+        private string GetThumbnailFullPath(FsArtifact fsArtifact)
+        {
+            var imagePath = fsArtifact.FullPath;
+            var lastModifiedDateTimeTicksStr = fsArtifact.LastModifiedDateTime.UtcTicks.ToString();
+            var finalName = imagePath + lastModifiedDateTimeTicksStr;
+
+            var imagePathHash = MakeHashData.ComputeSha256Hash(finalName);
+            var destinationDirectory = Path.Combine(MauiApplication.Current.CacheDir.Path, "FxThumbFolder");
+
+            if (!Directory.Exists(destinationDirectory))
+            {
+                Directory.CreateDirectory(destinationDirectory);
+            }
+
+            var thumbPath = Path.Combine(destinationDirectory, Path.ChangeExtension(imagePathHash, "Jpeg"));
+
+            return thumbPath;
         }
     }
 }
--- Functionland.FxFiles/Web/Pages/HomePage.razor.cs
+++ Functionland.FxFiles/Web/Pages/HomePage.razor.cs
@@ -8,9 +8,6 @@ namespace Functionland.FxFiles.App.Pages;
 
 public partial class HomePage
 {
-    private string internalfilePath2;
-
-    [AutoInject] public IFileService FileService { get; set; }
     private void NavigateToTestExplorerComponent()
     {
         NavigationManager.NavigateTo("/TestExplorer");
