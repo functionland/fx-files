2f555c93a5c0e03c9e253344f09931a4cd663f5a
Zahra Bayat <zahra.bayat13799@gmail.com>
2022-09-20 19:49:28 +0430

    Add AndroidThumbnailService. ab#238

---
+++ Functionland.FxFiles/App/Platforms/Android/Implementations/AndroidThumbnailService.cs
@@ -0,0 +1,20 @@
+﻿using Functionland.FxFiles.Shared.Models;
+using Functionland.FxFiles.Shared.Utils;
+using android = Android;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.Graphics;
+
+namespace Functionland.FxFiles.App.Platforms.Android.Implementations
+{
+    public class AndroidThumbnailService : LocalThumbnailService
+    {
+        public override Task<string> MakeThumbnailAsync(FsArtifact fsArtifact, CancellationToken? cancellationToken = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
