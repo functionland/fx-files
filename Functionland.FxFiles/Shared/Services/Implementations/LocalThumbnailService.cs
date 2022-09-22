33758b7f025045098e59c45c4dff6c031f9f7096
Ziba Seyfaei <ziba.seifaie@outlook.com>
2022-09-22 21:13:07 +0330

    Enhance ThumbnailService. ab#239
    Co-Authored-By: siroossha <113217130+siroossha@users.noreply.github.com>

--- Functionland.FxFiles/Shared/Services/Implementations/LocalThumbnailService.cs
+++ Functionland.FxFiles/Shared/Services/Implementations/LocalThumbnailService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Functionland.FxFiles.Shared.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -10,5 +11,26 @@ namespace Functionland.FxFiles.Shared.Services.Implementations
     public abstract class LocalThumbnailService : IThumbnailService
     {
         public abstract Task<string> MakeThumbnailAsync(FsArtifact fsArtifact, CancellationToken? cancellationToken = null);
+
+        public abstract string GetAppCacheDirectory();
+
+        public virtual string GetThumbnailFullPath(FsArtifact fsArtifact)
+        {
+            var imagePath = fsArtifact.FullPath;
+            var lastModifiedDateTimeTicksStr = fsArtifact.LastModifiedDateTime.UtcTicks.ToString();
+            var finalName = imagePath + lastModifiedDateTimeTicksStr;
+
+            var imagePathHash = MakeHashData.ComputeSha256Hash(finalName);
+            var destinationDirectory = Path.Combine(GetAppCacheDirectory(), "FxThumbFolder");
+
+            if (!Directory.Exists(destinationDirectory))
+            {
+                Directory.CreateDirectory(destinationDirectory);
+            }
+
+            var thumbPath = Path.Combine(destinationDirectory, Path.ChangeExtension(imagePathHash, "Jpeg"));
+
+            return thumbPath;
+        }
     }
 }
