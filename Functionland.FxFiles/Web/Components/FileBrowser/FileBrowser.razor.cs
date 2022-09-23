daac32a9c43bb64f105cf4ca8caef47cd3074531
Ali Muhammadi <alimuhammadiii@outlook.com>
2022-09-23 12:48:20 +0330

    fix conflict
    Co-Authored-By: Pegah <2495769+pegah-m-sadeghi@users.noreply.github.com>
    Co-Authored-By: negar-kln <113377124+negar-kln@users.noreply.github.com>
    Co-Authored-By: siroossha <113217130+siroossha@users.noreply.github.com>
    Co-Authored-By: Hootan HT <hootanhemmati@outlook.com>

--- Functionland.FxFiles/Web/Components/FileBrowser/FileBrowser.razor.cs
+++ Functionland.FxFiles/Web/Components/FileBrowser/FileBrowser.razor.cs
@@ -1,4 +1,9 @@
 ﻿using Functionland.FxFiles.App.Components.Common;
+using Functionland.FxFiles.App.Components.DesignSystem;
+using Functionland.FxFiles.App.Components.Modal;
+using Functionland.FxFiles.Shared.Services.Contracts;
+
+using Microsoft.Extensions.Localization;
 
 namespace Functionland.FxFiles.App.Components;
 
@@ -7,6 +12,8 @@ public partial class FileBrowser
     private FsArtifact? _currentArtifact;
     private List<FsArtifact> _pins = new();
     private List<FsArtifact> _artifacts = new();
+    private ArtifactSelectionModal? _artifactSelectionModalRef { get; set; }
+    private ArtifactOverflowModal _asm { get; set; }
 
     [Parameter] public IPinService PinService { get; set; } = default!;
 
@@ -19,6 +26,102 @@ public partial class FileBrowser
         await LoadChildrenArtifactsAsync();
 
         await base.OnInitAsync();
+    }
+
+    public async Task HandleCopyArtifactsAsync(List<FsArtifact> artifacts, CancellationToken cancellationToken)
+    {
+        List<FsArtifact> existArtifacts = new();
+        string? destinationPath = await HandleSelectDestinationArtifact();
+        if (string.IsNullOrWhiteSpace(destinationPath))
+        {
+            return;
+        }
+
+        try
+        {
+            await FileService.CopyArtifactsAsync(artifacts.ToArray(), destinationPath, false, cancellationToken);
+        }
+        catch (CanNotOperateOnFilesException ex)
+        {
+            existArtifacts = ex.FsArtifacts;
+        }
+
+        if (existArtifacts.Any())
+        {
+            //TODO: handle skip or replace artifactsToCopy
+        }
+    }
+
+    public async Task HandleMoveArtifactsAsync(List<FsArtifact> artifacts, CancellationToken cancellationToken)
+    {
+        List<FsArtifact> existArtifacts = new();
+        string? destinationPath = await HandleSelectDestinationArtifact();
+        if (string.IsNullOrWhiteSpace(destinationPath))
+        {
+            return;
+        }
+
+        try
+        {
+            await FileService.CopyArtifactsAsync(artifacts.ToArray(), destinationPath, false, cancellationToken);
+        }
+        catch (CanNotOperateOnFilesException ex)
+        {
+            existArtifacts = ex.FsArtifacts;
+        }
+
+        if (existArtifacts.Any())
+        {
+            //TODO: handle skip or replace artifactsToMove
+        }
+
+    }
+
+    public async Task<string?> HandleSelectDestinationArtifact()
+    {
+        var Result = await _artifactSelectionModalRef?.ShowAsync();
+        string? destinationPath = null;
+
+        if (Result?.ResultType == ArtifactSelectionResultType.Ok)
+        {
+            var destinationFsArtifact = Result.SelectedArtifacts.FirstOrDefault();
+            destinationPath = destinationFsArtifact?.FullPath;
+        }
+
+        return destinationPath;
+    }
+
+    public async Task HandleRenameArtifact(FsArtifact artifact, string newName, CancellationToken cancellationToken)
+    {
+        string filePath = artifact.FullPath;
+        try
+        {
+            await FileService.RenameFileAsync(filePath, newName, cancellationToken);
+        }
+        catch (DomainLogicException ex) when (ex.Message == Localizer.GetString(AppStrings.ArtifactPathIsNull, "file"))
+        {
+            //TODO: show exeception message with toast
+        }
+    }
+
+    public void HandlePinArtifacts()
+    {
+
+    }
+
+    public void HandleDeleteArtifacts()
+    {
+
+    }
+
+    public void HandleShowDetailsArtifact()
+    {
+
+    }
+
+    public void HandleCreateFolder()
+    {
+
     }
 
     private async Task LoadPinsAsync()
@@ -54,6 +157,16 @@ public partial class FileBrowser
         _currentArtifact = artifact;
         await LoadChildrenArtifactsAsync(_currentArtifact);
         // load current artifacts
+    }
+
+    private async Task HandleOptionsArtifact(FsArtifact artifact)
+    {
+        await _asm.ShowAsync();
+    }
+
+    private async Task HandleSelectedArtifactsOptions(List<FsArtifact> artifact)
+    {
+        await _asm.ShowAsync();
     }
 
     private bool IsInRoot(FsArtifact? artifact)
