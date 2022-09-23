112014b853f5aecd05c5ee9dd28e357a1c665e2f
Hootan Hemmati <hootanhemmati@outlook.com>
2022-09-23 19:38:29 +0330

    Modify ToastModal ab#330
    Co-Authored-By: Pegah <2495769+pegah-m-sadeghi@users.noreply.github.com>
    Co-Authored-By: negar-kln <113377124+negar-kln@users.noreply.github.com>
    Co-Authored-By: siroossha <113217130+siroossha@users.noreply.github.com>

--- Functionland.FxFiles/Web/Pages/HomePage.razor.cs
+++ Functionland.FxFiles/Web/Pages/HomePage.razor.cs
@@ -3,12 +3,16 @@ using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 using Functionland.FxFiles.App.Components.Common;
+using Functionland.FxFiles.App.Components.DesignSystem;
+using Functionland.FxFiles.App.Components.Modal;
 
 namespace Functionland.FxFiles.App.Pages;
 
 public partial class HomePage
 {
     public bool IsBottomSheetClose { get; set; } = false;
+
+    private ToastModal _toast { get; set; } = default!;
 
     public void SyncCloseState(bool isClose)
     {
@@ -17,7 +21,7 @@ public partial class HomePage
 
     public void OpenSheet()
     {
-        IsBottomSheetClose = true;
+        _toast.Show("salam", "salam", FxToastType.Info);
     }
 }
 
