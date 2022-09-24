0324b59dfb9eb75c35819e0f4e554428a782dfa8
Hootan Hemmati <hootanhemmati@outlook.com>
2022-09-24 10:14:58 +0330

    Fix error build for partial class in component ab#335

--- Functionland.FxFiles/Web/Components/Modal/ArtifactDetailModal/ArtifactDetailModal.razor.cs
+++ Functionland.FxFiles/Web/Components/Modal/ArtifactDetailModal/ArtifactDetailModal.razor.cs
@@ -1,6 +1,87 @@
-﻿namespace Functionland.FxFiles.App.Components.Modal.ArtifactDetailModal
+﻿namespace Functionland.FxFiles.App.Components.Modal
 {
-    public class ArtifactDetailModal
+    public partial class ArtifactDetailModal
     {
+        [AutoInject]
+        private IFileService _fileService = default!;
+
+        private List<FsArtifact> _artifacts = default!;
+
+        private TaskCompletionSource<ArtifactDetailModalResult>? _tcs;
+
+        private bool _isModalOpen;
+
+        public bool IsMultiple { get; set; }
+
+        public void Download()
+        {
+            var result = new ArtifactDetailModalResult();
+            result.ResultType = ArtifactDetailModalResultType.Download;
+
+            _tcs!.SetResult(result);
+            _tcs = null;
+
+            _isModalOpen = false;
+        }
+
+        public void Move()
+        {
+            var result = new ArtifactDetailModalResult();
+            result.ResultType = ArtifactDetailModalResultType.Move;
+
+            _tcs!.SetResult(result);
+            _tcs = null;
+
+            _isModalOpen = false;
+        }
+
+        public void Pin()
+        {
+            var result = new ArtifactDetailModalResult();
+            result.ResultType = ArtifactDetailModalResultType.Pin;
+
+            _tcs!.SetResult(result);
+            _tcs = null;
+
+            _isModalOpen = false;
+        }
+
+        public void More()
+        {
+            var result = new ArtifactDetailModalResult();
+            result.ResultType = ArtifactDetailModalResultType.More;
+
+            _tcs!.SetResult(result);
+            _tcs = null;
+
+            _isModalOpen = false;
+        }
+
+        public async Task<ArtifactDetailModalResult> ShowAsync(List<FsArtifact> artifacts, bool isMultiple = false)
+        {
+            _tcs?.SetCanceled();
+            IsMultiple = isMultiple;
+            _isModalOpen = true;
+            StateHasChanged();
+
+            _tcs = new TaskCompletionSource<ArtifactDetailModalResult>();
+            return await _tcs.Task;
+        }
+
+        private void Close()
+        {
+            var result = new ArtifactDetailModalResult();
+            result.ResultType = ArtifactDetailModalResultType.Close;
+
+            _tcs!.SetResult(result);
+            _tcs = null;
+
+            _isModalOpen = false;
+        }
+
+        public void Dispose()
+        {
+            _tcs?.SetCanceled();
+        }
     }
 }
---
+++ Functionland.FxFiles/Web/Components/Modal/ArtifactDetailModal/ArtifactOverflowResultType.cs
@@ -0,0 +1,11 @@
+﻿namespace Functionland.FxFiles.App.Components.Modal
+{
+    public enum ArtifactDetailModalResultType
+    {
+        Download,
+        Move,
+        Pin,
+        More,
+        Close
+    }
+}
