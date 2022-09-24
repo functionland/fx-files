a451afd5058feccbe099300201564f5a2781a55e
Hootan Hemmati <hootanhemmati@outlook.com>
2022-09-24 22:41:47 +0330

    Add sample of ArtifactDetailModal component
    Co-Authored-By: Pegah <2495769+pegah-m-sadeghi@users.noreply.github.com>
    Co-Authored-By: negar-kln <113377124+negar-kln@users.noreply.github.com>
    Co-Authored-By: siroossha <113217130+siroossha@users.noreply.github.com>

--- Functionland.FxFiles/Web/Pages/HomePage.razor.cs
+++ Functionland.FxFiles/Web/Pages/HomePage.razor.cs
@@ -37,7 +37,25 @@ public partial class HomePage
         {
             artifacts.Add(item);
         }
-        var result = await _detailModal.ShowAsync(artifacts);
+        var result = await _detailModal.ShowAsync(artifacts, true);
+        switch (result.ResultType)
+        {
+            case ArtifactDetailModalResultType.Download:
+                _toast.Show("Download", "Download", FxToastType.Info);
+                break;
+
+            case ArtifactDetailModalResultType.Move:
+                _toast.Show("Move", "Move", FxToastType.Info);
+                break;
+
+            case ArtifactDetailModalResultType.Pin:
+                _toast.Show("Pin", "Pin", FxToastType.Info);
+                break;
+
+            case ArtifactDetailModalResultType.More:
+                _toast.Show("More", "More", FxToastType.Info);
+                break;
+        }
     }
 }
 
