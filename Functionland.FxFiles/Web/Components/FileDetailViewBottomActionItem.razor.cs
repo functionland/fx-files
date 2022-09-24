eea33f883013de8d5d4dbbf980bade784d0d52d5
Hootan Hemmati <hootanhemmati@outlook.com>
2022-09-24 22:43:03 +0330

    Modify FileDetailViewBottomActionItem component ab#335
    Co-Authored-By: Pegah <2495769+pegah-m-sadeghi@users.noreply.github.com>
    Co-Authored-By: negar-kln <113377124+negar-kln@users.noreply.github.com>
    Co-Authored-By: siroossha <113217130+siroossha@users.noreply.github.com>

--- Functionland.FxFiles/Web/Components/FileDetailViewBottomActionItem.razor.cs
+++ Functionland.FxFiles/Web/Components/FileDetailViewBottomActionItem.razor.cs
@@ -1,9 +1,14 @@
-﻿namespace Functionland.FxFiles.App.Components
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace Functionland.FxFiles.App.Components
 {
     public partial class FileDetailViewBottomActionItem
     {
         [Parameter, EditorRequired]
         public ActionIcon ActionIcon { get; set; }
+
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClick { get; set; }
     }
 
     public enum ActionIcon
