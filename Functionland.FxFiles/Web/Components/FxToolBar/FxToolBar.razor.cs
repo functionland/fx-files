a7f2a7bddb65e7dedae84c00c9228e14900791b5
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2022-09-25 11:57:26 +0330

    Fix FxToolbar on other pages
    Co-Authored-By: siroossha <113217130+siroossha@users.noreply.github.com>
    Co-Authored-By: negar-kln <113377124+negar-kln@users.noreply.github.com>
    Co-Authored-By: Pegah <2495769+pegah-m-sadeghi@users.noreply.github.com>

--- Functionland.FxFiles/Web/Components/FxToolBar/FxToolBar.razor.cs
+++ Functionland.FxFiles/Web/Components/FxToolBar/FxToolBar.razor.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 
-namespace Functionland.FxFiles.App.Components.FxToolBar
+namespace Functionland.FxFiles.App.Components
 {
     public partial class FxToolBar
     {
@@ -8,9 +8,9 @@ namespace Functionland.FxFiles.App.Components.FxToolBar
         [Parameter] public bool IsPinned { get; set; }
         [Parameter] public string? SubTitle { get; set; }
         [Parameter, EditorRequired] public bool IsInRoot { get; set; }
-        [Parameter] public bool IsAddButtonVisible { get; set; }
-        [Parameter] public bool IsBackButtonVisible { get; set; }
-        [Parameter] public bool IsOverflowButtonVisible { get; set; }
+        [Parameter] public bool IsAddButtonVisible { get; set; } = true;
+        [Parameter] public bool IsBackButtonVisible { get; set; } = true;
+        [Parameter] public bool IsOverflowButtonVisible { get; set; } = true;
         [Parameter] public EventCallback<MouseEventArgs> OnAddButtonClick { get; set; }
         [Parameter] public EventCallback<MouseEventArgs> OnOverflowButtonClick { get; set; }
         [Parameter] public EventCallback OnSearchFocused { get; set; }
