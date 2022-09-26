125f96afb47b2c7032a2b0affa2a8075f8cf0e72
Hamed Hajiloo <45947371+hamedhajiloo@users.noreply.github.com>
2022-09-27 00:55:50 +0330

    Add FsZone. ab#227
    Co-Authored-By: Pegah <2495769+pegah-m-sadeghi@users.noreply.github.com>
    Co-Authored-By: negar-kln <113377124+negar-kln@users.noreply.github.com>
    Co-Authored-By: siroossha <113217130+siroossha@users.noreply.github.com>

--- src/Client/Shared/Models/FsZone.cs
+++ src/Client/Shared/Models/FsZone.cs
@@ -2,8 +2,18 @@
 {
     public class FsZone
     {
+        public FsZone(string name)
+        {
+            var currentEpochTime = DateTimeOffset.Now.ToUnixTimeSeconds();
+            Name = name;
+            CreateEpochTime = currentEpochTime;
+            UpdateEpochTime = currentEpochTime;
+        }
 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long CreateEpochTime { get; set; }
+        public long UpdateEpochTime { get; set; }
+        public string? SharedWithDId { get; set; }
     }
-
-
 }
