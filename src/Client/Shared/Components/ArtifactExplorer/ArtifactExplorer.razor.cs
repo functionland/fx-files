456fec708084f8ddb30b0c2dd4b6b8158a66c4bf
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2022-09-26 11:27:19 +0330

    Fix ArtifacetExplorer file icons ab#319
    Co-Authored-By: siroossha <113217130+siroossha@users.noreply.github.com>
    Co-Authored-By: negar-kln <113377124+negar-kln@users.noreply.github.com>
    Co-Authored-By: Pegah <2495769+pegah-m-sadeghi@users.noreply.github.com>

--- src/Client/Shared/Components/ArtifactExplorer/ArtifactExplorer.razor.cs
+++ src/Client/Shared/Components/ArtifactExplorer/ArtifactExplorer.razor.cs
@@ -132,8 +132,32 @@
 
         public string GetArtifactIcon(FsArtifact artifact)
         {
-            //todo: Proper icon for artifact
-            return "text-file-icon";
+            if (artifact.ArtifactType == FsArtifactType.File)
+            {
+                switch (artifact.FileCategory)
+                {
+                    case FileCategoryType.Document:
+                        return "text-file-icon";
+                        break;
+                    case FileCategoryType.Other:
+                        return "text-file-icon";
+                        break;
+                    case FileCategoryType.Pdf:
+                        return "text-file-icon";
+                        break;
+                    case FileCategoryType.Image:
+                        return "photo-file-icon";
+                        break;
+                    case FileCategoryType.Audio:
+                        return "audio-file-icon";
+                        break;
+                    case FileCategoryType.Video:
+                        return "video-file-icon";
+                        break;
+                }
+            }
+
+            return "folder-icon";
         }
 
         public string GetArtifactSubText(FsArtifact artifact)
@@ -147,6 +171,5 @@
             OnFilterClick.InvokeAsync();
         }
     }
-
 }
 
