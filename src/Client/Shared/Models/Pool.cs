2df9043a0f3d36963e374bf928a5638d22dd0565
Hamed Hajiloo <45947371+hamedhajiloo@users.noreply.github.com>
2022-09-28 11:18:58 +0330

    Change type of Pool properties. ab#227

--- src/Client/Shared/Models/Pool.cs
+++ src/Client/Shared/Models/Pool.cs
@@ -1,18 +1,22 @@
-﻿namespace Functionland.FxFiles.Client.Shared.Models
+﻿namespace Functionland.FxFiles.Client.Shared.Models;
+
+public class Pool
 {
-    public class Pool
+    public Pool(string name, PoolType poolType)
     {
-        public int? Id { get; set; }
-        public string? Name { get; set; }
-        public PoolType? PoolType { get; set; }
-        public long? LastUpdate { get; set; }
-        public decimal? MonthlyRate { get; set; }
-        public decimal? CurrentUse { get; set; }
-        public float? PhotosUsed { get; set; }
-        public float? VideosUsed { get; set; }
-        public float? AudiosUsed { get; set; }
-        public float? DocsUsed { get; set; }
-        public float? OtherUsed { get; set; }
-        public string? AdditinalInformation { get; set; } //KeyValuePair<string, string>
+        Name = name;
+        PoolType = poolType;
     }
+    public int? Id { get; set; }
+    public string Name { get; set; }
+    public PoolType PoolType { get; set; }
+    public long? UpdateEpochTime { get; set; }
+    public decimal? MonthlyRate { get; set; }
+    public long CurrentUse { get; set; }
+    public long PhotosUsed { get; set; }
+    public long VideosUsed { get; set; }
+    public long AudiosUsed { get; set; }
+    public long DocsUsed { get; set; }
+    public long OtherUsed { get; set; }
+    public string? AdditinalInformation { get; set; } //KeyValuePair<string, string>
 }
