20c965a4d0308ed38d25300bbfd79c8ef3e8cf66
Hamed Hajiloo <45947371+hamedhajiloo@users.noreply.github.com>
2022-09-28 11:18:01 +0330

    Change type of Blox properties. ab#227

--- src/Client/Shared/Models/Blox.cs
+++ src/Client/Shared/Models/Blox.cs
@@ -1,17 +1,22 @@
-﻿namespace Functionland.FxFiles.Client.Shared.Models
+﻿namespace Functionland.FxFiles.Client.Shared.Models;
+
+public class Blox
 {
-    public class Blox
+    public Blox(string name,string ownerId)
     {
-        public int? Id { get; set; }
-        public string? Name { get; set; }
-        public string? OwnerId { get; set; }
-        public string? PoolName { get; set; }
-        public long? AvailableSpace { get; set; }
-        public long? CurrentUse { get; set; }
-        public long? PhotosUsed { get; set; }
-        public long? VideosUsed { get; set; }
-        public long? AudiosUsed { get; set; }
-        public long? DocsUsed { get; set; }
-        public long? OtherUsed { get; set; }
+        Name = name;
+        OwnerId = ownerId;
     }
+
+    public int? Id { get; set; }
+    public string Name { get; set; }
+    public string OwnerId { get; set; }
+    public string? PoolName { get; set; }
+    public long AvailableSpace { get; set; }
+    public long CurrentUse { get; set; }
+    public long PhotosUsed { get; set; }
+    public long VideosUsed { get; set; }
+    public long AudiosUsed { get; set; }
+    public long DocsUsed { get; set; }
+    public long OtherUsed { get; set; }
 }
