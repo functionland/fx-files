331daa318ac6dcc43f6389223d83b8c503e367a5
Hamed Hajiloo <45947371+hamedhajiloo@users.noreply.github.com>
2022-09-29 23:49:32 +0330

    Add IFulaUserClient. ab#452

---
+++ src/Client/Shared/Services/FulaClient/Contracts/IFulaUserClient.cs
@@ -0,0 +1,11 @@
+﻿namespace Functionland.FxFiles.Client.Shared.Services.FulaClient.Contracts;
+
+public interface IFulaUserClient
+{
+    Task<FulaUser> GetMyUserAsync(string did, CancellationToken? cancellationToken = null);
+    Task<FulaUser> GetUserAsync(string myDid, string otherDid, CancellationToken? cancellationToken = null);
+    Task<List<FulaUser>> GetUsersAsync(string myDid, IEnumerable<string> otherDids, CancellationToken? cancellationToken = null);
+    Task<Stream> GetMyAvatarAsync(string did, CancellationToken? cancellationToken = null);
+    Task<Stream> GetAvatarAsync(string did, CancellationToken? cancellationToken = null);
+    Task<string> GetThumbnailUrlAsync(string did, CancellationToken? cancellationToken = null);
+}
