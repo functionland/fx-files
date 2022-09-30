4c5c5c64331f405254dc5448d7e8e3af85fb251d
Zahra Bayat <zahra.bayat13799@gmail.com>
2022-09-30 18:39:31 +0330

    Implement image view for pin component #376

---
+++ src/Client/Shared/Services/Implementations/FxFileProvider.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functionland.FxFiles.Client.Shared.Services.Implementations
+{
+    public class FxFileProvider : IFileProvider
+    {
+        private readonly IFileProvider _fileProvider;
+
+        public FxFileProvider(IFileProvider fileProvider)
+        {
+            _fileProvider = fileProvider;
+        }
+
+        public IDirectoryContents GetDirectoryContents(string subpath)
+        {
+            return _fileProvider.GetDirectoryContents(subpath);
+        }
+
+        public IFileInfo GetFileInfo(string subpath)
+        {
+            if (subpath.EndsWith(".HandleByApp=true"))
+            {
+                subpath = subpath.Replace(".HandleByApp=true", string.Empty);
+                subpath = subpath.Replace("_content/Functionland.FxFiles.Client.Shared/", string.Empty);
+
+                return new ContactFileInfo(subpath);
+            }
+
+            return _fileProvider.GetFileInfo(subpath);
+        }
+
+        public IChangeToken Watch(string filter)
+        {
+            return _fileProvider.Watch(filter);
+        }
+    }
+}
+
+public class ContactFileInfo : IFileInfo
+{
+    private readonly string _path;
+
+    public ContactFileInfo(string path)
+    {
+        _path = path;
+    }
+
+    public bool Exists => true;
+
+    public long Length => 0;
+
+    public string PhysicalPath => null;
+
+    public string Name => null;
+
+    public DateTimeOffset LastModified => DateTimeOffset.FromUnixTimeSeconds(0);
+
+    public bool IsDirectory => false;
+
+    public Stream CreateReadStream()
+    {
+        try
+        {
+            var streamReader = new StreamReader(_path);
+            return streamReader.BaseStream;
+        }
+        catch (System.IO.FileNotFoundException)
+        {
+            return new MemoryStream();
+        }
+    }
+}
