3308e575cc192f711be2ca744dbd8f9df92f3065
Hootan Hemmati <hootanhemmati@outlook.com>
2022-10-01 09:33:43 +0330

    Add toast functionality to close automatically in 5 second after showed. ab#510

--- src/Client/Shared/Components/Modal/ToastModal/ToastModal.razor.cs
+++ src/Client/Shared/Components/Modal/ToastModal/ToastModal.razor.cs
@@ -1,4 +1,9 @@
-﻿namespace Functionland.FxFiles.Client.Shared.Components.Modal
+﻿using System.Threading;
+using System.Timers;
+
+using Timer = System.Timers.Timer;
+
+namespace Functionland.FxFiles.Client.Shared.Components.Modal
 {
     public partial class ToastModal
     {
@@ -17,8 +22,28 @@
             _toastType = toastType;
             _isModalOpen = true;
             StateHasChanged();
+            // Add timer for closing the modal after 5 seconds
+            var timer = new Timer(5000);
+            timer.Elapsed += OnTimedEvent!;
+            timer.Enabled = true;
+            StateHasChanged();
         }
 
+        private void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            TimerHasChangedForCloseAsync();
+        }
+        private async Task TimerHasChangedForCloseAsync()
+        {
+            if (_isModalOpen)
+            {
+                await InvokeAsync(() =>
+                {
+                    Close();
+                    StateHasChanged();
+                });
+            }
+        }
         private void Close()
         {
             _isModalOpen = false;
