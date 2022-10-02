a9c8ff9674be314a934f2ce022aef7c98f402f0d
Mehran Davoudi <mehran.davoudi@hotmail.com>
2022-10-02 14:39:20 +0330

    Add the new FileProvider architecture sketch. ab#1538

--- src/Client/Shared/Services/Implementations/FxFileProvider.cs
+++ src/Client/Shared/Services/Implementations/FxFileProvider.cs
@@ -25,14 +25,19 @@ namespace Functionland.FxFiles.Client.Shared.Services.Implementations
 
         public IFileInfo GetFileInfo(string subpath)
         {
-            if (subpath.EndsWith(".HandleByApp=true"))
-            {
-                subpath = subpath.Replace("%20", " ");
-                subpath = subpath.Replace(".HandleByApp=true", string.Empty);
-                subpath = subpath.Replace("_content/Functionland.FxFiles.Client.Shared/", string.Empty);
+            if (subpath.StartsWith("fula://"))
+                return new FulaFileInfo(PreparePath(subpath));
+            else if (subpath.StartsWith("storage://") || subpath.StartsWith("wwwroot://"))
+                return new StorageFileInfo(PreparePath(subpath));
 
-                return new FxFileInfo(subpath);
-            }
+            //if (subpath.EndsWith(".HandleByApp=true"))
+            //{
+            //    subpath = subpath.Replace("%20", " ");
+            //    subpath = subpath.Replace(".HandleByApp=true", string.Empty);
+            //    subpath = subpath.Replace("_content/Functionland.FxFiles.Client.Shared/", string.Empty);
+
+            //    return new FxFileInfo(subpath);
+            //}
 
             return _fileProvider.GetFileInfo(subpath);
         }
@@ -41,14 +46,57 @@ namespace Functionland.FxFiles.Client.Shared.Services.Implementations
         {
             return _fileProvider.Watch(filter);
         }
+
+        private string PreparePath(string path)
+        {
+            path = path.Replace("fula://", string.Empty);
+            path = path.Replace("storage://", string.Empty);
+            path = path.Replace("wwwroot://", string.Empty);
+            return path;
+        }
     }
 }
 
-public class FxFileInfo : IFileInfo
+public class StorageFileInfo : IFileInfo
 {
     private readonly string _path;
 
-    public FxFileInfo(string path)
+    public StorageFileInfo(string path)
+    {
+        _path = path;
+    }
+
+    public bool Exists => true;
+
+    public long Length => 0;
+
+    public string PhysicalPath => null;
+
+    public string Name => null;
+
+    public DateTimeOffset LastModified => DateTimeOffset.FromUnixTimeSeconds(0);
+
+    public bool IsDirectory => false;
+
+    public Stream CreateReadStream()
+    {
+        try
+        {
+            var streamReader = new StreamReader(_path);
+            return streamReader.BaseStream;
+        }
+        catch
+        {
+            return new MemoryStream();
+        }
+    }
+}
+
+public class FulaFileInfo : IFileInfo
+{
+    private readonly string _path;
+
+    public FulaFileInfo(string path)
     {
         _path = path;
     }
