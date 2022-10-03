aa637dbd9880a606e8bad6a0eb10b053f7003459
Hootan Hemmati <hootanhemmati@outlook.com>
2022-10-03 23:31:47 +0330

    Fix functionality of check list down or not for show go to top button ab#375

--- src/Client/Shared/wwwroot/scripts/backToTop.js
+++ src/Client/Shared/wwwroot/scripts/backToTop.js
@@ -2,9 +2,15 @@
     document.documentElement.scrollTop = 0;
 }
 
+window.onscroll = function () { OnScrollCheck() };
+
 function OnScrollCheck() {
-    if (document.documentElement.scrollTop > 5) {
+    if (document.documentElement.scrollTop > 100) {
         var scrollButton = document.getElementsByClassName('position-scroll-btn')[0];
         scrollButton.style.display = 'block';
+    }
+    else {
+        var scrollButton = document.getElementsByClassName('position-scroll-btn')[0];
+        scrollButton.style.display = 'none';
     }
 }
