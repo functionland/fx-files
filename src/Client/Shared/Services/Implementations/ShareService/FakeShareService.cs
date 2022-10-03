af024c0248f68ddf5e0bf9b1c38cf854143f1fcf
Hamed Hajiloo <45947371+hamedhajiloo@users.noreply.github.com>
2022-10-03 21:37:57 +0330

    Add IShareService. ab#411
    Co-Authored-By: negar-kln <113377124+negar-kln@users.noreply.github.com>
    Co-Authored-By: Ziba Seyfaei <63063815+zibasfy@users.noreply.github.com>
    Co-Authored-By: siroossha <113217130+siroossha@users.noreply.github.com>
    Co-Authored-By: Pegah <2495769+pegah-m-sadeghi@users.noreply.github.com>

---
+++ src/Client/Shared/Services/Implementations/ShareService/FakeShareService.cs
@@ -0,0 +1,39 @@
+﻿namespace Functionland.FxFiles.Client.Shared.Services.Implementations;
+
+public class FakeShareService : IFulaShareService, ILocalDeviceShareService
+{
+    public Task EnsureInitializedAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IAsyncEnumerable<FsArtifact> GetSharedFsArtifactsAsync(CancellationToken? cancellationToken = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InitAsync(CancellationToken? cancellationToken = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task ShareFsArtifactAsync(IEnumerable<string> dids, FsArtifact fsArtifact, CancellationToken? cancellationToken = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task ShareFsArtifactsAsync(IEnumerable<string> dids, IEnumerable<FsArtifact> fsArtifact, CancellationToken? cancellationToken = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UnShareFsArtifactAsync(IEnumerable<string> dids, string artifactFullPath, CancellationToken? cancellationToken = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UnShareFsArtifactsAsync(IEnumerable<string> dids, IEnumerable<string> artifactFullPaths, CancellationToken? cancellationToken = null)
+    {
+        throw new NotImplementedException();
+    }
+}
