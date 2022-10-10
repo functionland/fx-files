5458a4dba50cc400d1d065634b01fda8265c84e5
Hamed Hajiloo <45947371+hamedhajiloo@users.noreply.github.com>
2022-10-10 16:51:53 +0330

    Add progress to IFulaClient. ab#411

--- src/Client/Shared/Services/FulaClient/Contracts/IFulaFileClient.cs
+++ src/Client/Shared/Services/FulaClient/Contracts/IFulaFileClient.cs
@@ -1,17 +1,17 @@
-﻿using Functionland.FxFiles.Client.Shared.Services.FulaClient.Common;
+﻿using Functionland.FxFiles.Client.Shared.Components.Modal;
 
 namespace Functionland.FxFiles.Client.Shared.Services.FulaClient.Contracts;
 
 public interface IFulaFileClient
 {
-    Task UploadFileAsync(string token, string path, Stream stream, Action<FileProgressEventArgs>? onProgress = null, CancellationToken? cancellationToken = null);
+    Task UploadFileAsync(string token, string path, Stream stream, Action<ProgressInfo>? onProgress = null, CancellationToken? cancellationToken = null);
     Task AddFolderAsync(string token, string path, string folderName, CancellationToken? cancellationToken = null);
-    Task<Stream> GetFileStreamAsync(string token, string filePath, CancellationToken? cancellationToken = null);
-    Task MoveArtifactsAsync(string token, IEnumerable<string> sourcesPath, string destinationPath, bool overwrite = false, CancellationToken? cancellationToken = null);
-    Task CopyArtifactsAsync(string token, IEnumerable<string> sourcesPath, string destinationPath, bool overwrite = false, CancellationToken? cancellationToken = null);
+    Task<Stream> GetFileStreamAsync(string token, string filePath, Action<ProgressInfo>? onProgress = null, CancellationToken? cancellationToken = null);
+    Task MoveArtifactsAsync(string token, IEnumerable<string> sourcesPath, string destinationPath, bool overwrite = false, Action<ProgressInfo>? onProgress = null, CancellationToken? cancellationToken = null);
+    Task CopyArtifactsAsync(string token, IEnumerable<string> sourcesPath, string destinationPath, bool overwrite = false, Action<ProgressInfo>? onProgress = null, CancellationToken? cancellationToken = null);
     Task RenameFileAsync(string token, string filePath, string newName, CancellationToken? cancellationToken = null);
     Task RenameFolderAsync(string token, string folderPath, string newName, CancellationToken? cancellationToken = null);
-    Task DeleteArtifactsAsync(string token, IEnumerable<string> sourcesPath, CancellationToken? cancellationToken = null);
+    Task DeleteArtifactsAsync(string token, IEnumerable<string> sourcesPath, Action<ProgressInfo>? onProgress = null, CancellationToken? cancellationToken = null);
     IAsyncEnumerable<FsArtifact> GetArtifactsAsync(string token, string? path = null, CancellationToken? cancellationToken = null);
     IAsyncEnumerable<FsArtifact> SearchArtifactsAsync(string token, string? path = null, string? searchText = null, CancellationToken? cancellationToken = null);
 
