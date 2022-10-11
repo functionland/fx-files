3cb01f3653b68117c7fbc50dcd12614aa1220865
Zahra Ahangari <ahangari95@gmail.com>
2022-10-11 17:30:31 +0330

    Fix camera image rotation issue in Windows. ab#548
    Co-Authored-By: Zahra Bayat <Zahra.bayat13799@gmail.com>

--- src/Client/App/Platforms/Windows/Implementations/WindowsThumbnailService.cs
+++ src/Client/App/Platforms/Windows/Implementations/WindowsThumbnailService.cs
@@ -1,6 +1,8 @@
 ﻿using Functionland.FxFiles.Client.Shared.Models;
 using Functionland.FxFiles.Client.Shared.Resources;
 using Functionland.FxFiles.Client.Shared.Utils;
+using System.Drawing;
+using Windows.System;
 
 namespace Functionland.FxFiles.Client.App.Platforms.Windows.Implementations;
 
@@ -15,6 +17,8 @@ public partial class WindowsThumbnailService : LocalThumbnailService
         if (File.Exists(thumbPath)) return thumbPath;
 
         var image = System.Drawing.Image.FromFile(fsArtifact.FullPath);
+
+        image = CorrectRotation(image);
 
         (int imageWidth, int imageHeight) = ImageUtils.ScaleImage(image.Width, image.Height, 252, 146);
 
@@ -35,4 +39,45 @@ public partial class WindowsThumbnailService : LocalThumbnailService
     {
         return FileSystem.CacheDirectory;
     }
+
+    public System.Drawing.Image CorrectRotation(System.Drawing.Image? img)
+    {
+        if (Array.IndexOf(img.PropertyIdList, 274) > -1)
+        {
+            var orientation = (int)img.GetPropertyItem(274).Value[0];
+            switch (orientation)
+            {
+                case 1:
+                    // No rotation required.
+                    break;
+                case 2:
+                    img.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                    break;
+                case 3:
+                    img.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                    break;
+                case 4:
+                    img.RotateFlip(RotateFlipType.Rotate180FlipX);
+                    break;
+                case 5:
+                    img.RotateFlip(RotateFlipType.Rotate90FlipX);
+                    break;
+                case 6:
+                    img.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                    break;
+                case 7:
+                    img.RotateFlip(RotateFlipType.Rotate270FlipX);
+                    break;
+                case 8:
+                    img.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                    break;
+            }
+            // This EXIF data is now invalid and should be removed.
+            img.RemovePropertyItem(274);
+
+        }
+
+        return img;
+    }
+
 }
