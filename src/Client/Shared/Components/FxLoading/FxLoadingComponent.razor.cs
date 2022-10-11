f393cf36f016b2e88d6cd66ddf4fb4ddc34ce2a8
Maryam Davoudi <davoudi.maryam@outlook.com>
2022-10-11 15:48:46 +0330

    fix loading ab#647

--- src/Client/Shared/Components/FxLoading/FxLoadingComponent.razor.cs
+++ src/Client/Shared/Components/FxLoading/FxLoadingComponent.razor.cs
@@ -1,6 +1,6 @@
 ﻿namespace Functionland.FxFiles.Client.Shared.Components;
 
-public partial class LoadingComponent
+public partial class FxLoadingComponent
 {
     [Parameter] public string Color { get; set; } = "#808080";
     [Parameter] public string ZIndex { get; set; } = "0";
