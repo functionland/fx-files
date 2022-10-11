432c3c4c43406a2a5cb6c8433b27a3dd9c3bff28
Zahra Bayat <zahra.bayat13799@gmail.com>
2022-10-11 17:28:43 +0330

    Fix camera images orientation issue ab#548
    Co-Authored-By: Zahra Ahangari <ahangari95@gmail.com>
    Co-Authored-By: Hootan HT <hootanhemmati@outlook.com>

--- src/Client/App/Platforms/Android/Implementations/AndroidThumbnailService.cs
+++ src/Client/App/Platforms/Android/Implementations/AndroidThumbnailService.cs
@@ -1,8 +1,21 @@
-﻿using Functionland.FxFiles.Client.Shared.Models;
+﻿using Android.Graphics;
+
+using AndroidX.ExifInterface.Media;
+
+using Functionland.FxFiles.Client.Shared.Models;
 using Functionland.FxFiles.Client.Shared.Utils;
 
+using Java.IO;
+
 using Microsoft.Maui.Graphics.Platform;
+
+using System.Drawing;
+
+using static Android.Webkit.WebStorage;
+
+using Bitmap = Android.Graphics.Bitmap;
 using IImage = Microsoft.Maui.Graphics.IImage;
+using Size = Microsoft.Maui.Graphics.Size;
 
 namespace Functionland.FxFiles.Client.App.Platforms.Android.Implementations;
 
@@ -12,23 +25,17 @@ public class AndroidThumbnailService : LocalThumbnailService
     {
         var thumbPath = GetThumbnailFullPath(fsArtifact);
 
-        if (File.Exists(thumbPath)) return thumbPath;
+        if (System.IO.File.Exists(thumbPath)) return thumbPath;
 
-        IImage image;
-        using var streamReader = new StreamReader(fsArtifact.FullPath);
-
-        using var stream = streamReader.BaseStream;
-        image = PlatformImage.FromStream(stream);
-
-        if (image != null)
+        var imageFile = new Java.IO.File(fsArtifact.FullPath);
+        var bitmap = BitmapFactory.DecodeFile(imageFile.AbsolutePath);
+        if (bitmap != null)
         {
-            (int imageWidth, int imageHeight) = ImageUtils.ScaleImage((int)image.Width, (int)image.Height, 252, 146);
-            IImage newImage = image.Downsize(imageWidth, imageHeight, true);
-            using MemoryStream memStream = new MemoryStream();
+            bitmap = CorrectOration(fsArtifact.FullPath, bitmap);
+            (int imageWidth, int imageHeight) = ImageUtils.ScaleImage((int)bitmap.Width, (int)bitmap.Height, 252, 146);
+            var newBitmap = bitmap.Downsize(imageWidth, imageHeight);
             using var fileStream = new FileStream(thumbPath, FileMode.Create);
-            newImage.Save(memStream);
-            memStream.Seek(0, SeekOrigin.Begin);
-            await memStream.CopyToAsync(fileStream);
+            await newBitmap.CompressAsync(Bitmap.CompressFormat.Jpeg, 1, fileStream);
         }
 
         return thumbPath;
@@ -37,5 +44,43 @@ public class AndroidThumbnailService : LocalThumbnailService
     public override string GetAppCacheDirectory()
     {
         return MauiApplication.Current.CacheDir.Path;
+    }
+
+    private Bitmap? CorrectOration(string photoPath, Bitmap bitmap)
+    {
+        ExifInterface ei = new ExifInterface(photoPath);
+        int orientation = ei.GetAttributeInt(ExifInterface.TagOrientation,
+                                             ExifInterface.OrientationUndefined);
+
+        Bitmap? rotatedBitmap = null;
+        switch (orientation)
+        {
+            case ExifInterface.OrientationRotate90:
+                rotatedBitmap = rotateImage(bitmap, 90);
+                break;
+
+            case ExifInterface.OrientationRotate180:
+                rotatedBitmap = rotateImage(bitmap, 180);
+                break;
+
+            case ExifInterface.OrientationRotate270:
+                rotatedBitmap = rotateImage(bitmap, 270);
+                break;
+
+            case ExifInterface.OrientationNormal:
+            default:
+                rotatedBitmap = bitmap;
+                break;
+        }
+
+        return rotatedBitmap;
+    }
+
+    public static Bitmap? rotateImage(Bitmap source, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.PostRotate(angle);
+        return Bitmap.CreateBitmap(source, 0, 0, source.Width, source.Height,
+                                   matrix, true);
     }
 }
