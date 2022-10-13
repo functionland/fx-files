328751d095d2642fc6403b138c310ec31607a82f
Maryam Davoudi <davoudi.maryam@outlook.com>
2022-10-14 02:26:24 +0330

    Improve folder structure ab#647

--- src/Client/Shared/Pages/_Onboarding/SetupCompletePage/SetupCompletePage.razor.cs
+++ src/Client/Shared/Pages/_Onboarding/SetupCompletePage/SetupCompletePage.razor.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Functionland.FxFiles.Client.Shared.Pages.Onboarding.SetupComplete
+namespace Functionland.FxFiles.Client.Shared.Pages
 {
-    public partial class SetupComplete
+    public partial class SetupCompletePage
     {
     }
 }
