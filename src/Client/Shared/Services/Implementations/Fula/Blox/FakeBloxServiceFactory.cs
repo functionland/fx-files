fa7179b514309d5efb60af6cdc2775963e51f53c
khanizahra <zahrakhanii@hotmail.com>
2022-10-13 22:49:09 +0330

    Edit FakeBloxServiceFactory. ab#718

--- src/Client/Shared/Services/Implementations/Fula/Blox/FakeBloxServiceFactory.cs
+++ src/Client/Shared/Services/Implementations/Fula/Blox/FakeBloxServiceFactory.cs
@@ -27,7 +27,7 @@ public partial class FakeBloxServiceFactory
         var blox = new FakeBloxService(
                 new List<Blox>
                 {
-                    CreateBlox("My Blox","The long number of characters in the blox id should be handled ", "Owner Name","1426")
+                    CreateBlox("My Blox","The long number of characters in the blox id should be handled ", "OwnerDId")
                 },
                 invitedBloxs,
                 actionLatency,
@@ -42,9 +42,9 @@ public partial class FakeBloxServiceFactory
         var invitedBloxs = new FakeBloxService(
             new List<Blox>
             {
-                CreateBlox("Fifth one", "Fifth Blox", "The long number of characters in the owner blox name should be handled"),
-                CreateBlox("Second Blox", "Second Blox","The long number of characters in the Owner name should be handled","Second Pool"),
-                CreateBlox("The long number of characters in the blox id should be handled", "Third Blox","Second Pool", "Second")
+                CreateBlox("Fifth one", "Fifth Blox", "The long number of characters in the OwnerDId should be handled"),
+                CreateBlox("Second Blox", "Second Blox","Second Blox OwnerDId"),
+                CreateBlox("The long number of characters in the blox id should be handled", "Third Blox","Third Blox OwnerDId")
             },
             bloxs,
             actionLatency,
@@ -60,9 +60,9 @@ public partial class FakeBloxServiceFactory
         var bloxs = new FakeBloxService(
             new List<Blox>
             {
-                CreateBlox("The long number of characters in the blox id should be handled", "The long number of characters in the Blox name should be handled","Blox Owner"),
-                CreateBlox("Home Blox Id", "Home Blox Name", "Home Blox Owner", "Home Blox PoolId"),
-                CreateBlox("Company Blox", "Company Blox","The long number of characters in the owner blox name should be handled","Company Blox PoolId")
+                CreateBlox("The long number of characters in the blox id should be handled", "The long number of characters in the Blox name should be handled","Blox OwnerDId"),
+                CreateBlox("Home Blox Id", "Home Blox Name", "Home Blox Owner"),
+                CreateBlox("Company Blox", "Company Blox","Company Blox OwnerDId")
             },
             invitedBloxs,
             actionLatency,
@@ -76,26 +76,26 @@ public partial class FakeBloxServiceFactory
         var bloxs = new FakeBloxService(
             new List<Blox>
             {
-                CreateBlox("My Country Blox One", "First Blox", "First Blox Owner" , "First"),
-                CreateBlox("My Country Blox Two", "Second Blox","Second Blox Owner", "Second"),
-                CreateBlox("My Country Blox Three", "Third Blox","Third Blox Owner", "Third"),
-                CreateBlox("My City Blox1", "My City  Blox1", "My City  Blox Owner", "PoolId 12365"),
-                CreateBlox("Eighth Blox One", "Eighth Blox One", "Eighth Owner Name", "Eighth11111"),
-                CreateBlox("Eighth Blox Two", "Eighth Blox Two", "Eighth Owner Name", "Eighth12222"),
-                CreateBlox("Canada Blox", "Canada Blox", "Canada Owner Name", "Canada"),
-                CreateBlox("Germany Blox", "Germany Blox", "Germany Owner Name", "Germany"),
-                CreateBlox("China Blox", "China Blox", "China Owner Name", "China"),
-                CreateBlox("Home Blox", "Home Blox", "Home Owner Name", "Home"),
-                CreateBlox("Company Blox", "Company Blox", "Company Owner Name", "Company")
+                CreateBlox("My Country Blox One", "First Blox", "First Blox OwnerDId"),
+                CreateBlox("My Country Blox Two", "Second Blox","Second Blox OwnerDId"),
+                CreateBlox("My Country Blox Three", "Third Blox","Third Blox OwnerDId"),
+                CreateBlox("My City Blox1", "My City  Blox1", "My City  Blox OwnerDId"),
+                CreateBlox("Eighth Blox One", "Eighth Blox One", "Eighth OwnerDId"),
+                CreateBlox("Eighth Blox Two", "Eighth Blox Two", "Eighth OwnerDId"),
+                CreateBlox("Canada Blox", "Canada Blox", "Canada OwnerDId"),
+                CreateBlox("Germany Blox", "Germany Blox", "Germany OwnerDId"),
+                CreateBlox("China Blox", "China Blox", "China OwnerDId"),
+                CreateBlox("Home Blox", "Home Blox", "Home OwnerDId"),
+                CreateBlox("Company Blox", "Company Blox", "Company OwnerDId")
             },
             new List<Blox>
             {
-                CreateBlox("My City InvitedBloxs", "My City Blox", "My City", "My City Pool"),
-                CreateBlox("My Father InvitedBloxs", "My Father Blox", "12364"),
-                CreateBlox("My Friend InvitedBloxs", "My Friend Blox","Canada", "1452Canada"),
-                CreateBlox("The long number of characters in the InvitedBloxs name should be handled ", "InvitedBloxs", "Germany" , "1254Germany"),
-                CreateBlox("Company InvitedBloxs", "Company InvitedBloxs", "Company Owner Name", "Company"),
-                CreateBlox("China InvitedBloxs", "China InvitedBloxs", "China InvitedBloxs Owner Name", "ChinaInvitedBloxs"),
+                CreateBlox("My City InvitedBloxs", "My City Blox", "My City"),
+                CreateBlox("My Father InvitedBloxs", "My Father Blox","OwnerDId"),
+                CreateBlox("My Friend InvitedBloxs", "My Friend Blox", "1452Canada"),
+                CreateBlox("The long number of characters in the InvitedBloxs name should be handled ", "InvitedBloxs", "Germany"),
+                CreateBlox("Company InvitedBloxs", "Company InvitedBloxs", "Company"),
+                CreateBlox("China InvitedBloxs", "China InvitedBloxs", "ChinaInvitedBloxs"),
             },
             actionLatency,
             enumerationLatency);
@@ -104,14 +104,10 @@ public partial class FakeBloxServiceFactory
     }
 
     private static Blox CreateBlox(string bloxId,
-                                  string name,
-                                  string ownerName,
-                                  string? poolId = null)
+                                   string name,
+                                   string ownerDId)
     {
-        var blox = new Blox(bloxId, name, ownerName)
-        {
-            PoolId = poolId,
-        };
+        var blox = new Blox(bloxId, name, ownerDId);
 
         return blox;
     }
