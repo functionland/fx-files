f672936615cc9a53bb7355c2b9edf109e87f4531
Zahra Bayat <zahra.bayat13799@gmail.com>
2022-10-14 19:39:33 +0330

    Separate fulaFileInfo and StorageFileInfo ab#538

---
+++ src/Client/Shared/Services/Implementations/FxFileInfo/FulaFileInfo.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functionland.FxFiles.Client.Shared.Services.Implementations.FxFileInfo;
+
+public class FulaFileInfo : IFileInfo
+{
+    private readonly string _path;
+
+    public FulaFileInfo(string path)
+    {
+        _path = path;
+    }
+
+    public bool Exists => true;
+
+    public long Length => 0;
+
+    public string PhysicalPath => null;
+
+    public string Name => null;
+
+    public DateTimeOffset LastModified => DateTimeOffset.FromUnixTimeSeconds(0);
+
+    public bool IsDirectory => false;
+
+    public Stream CreateReadStream()
+    {
+        try
+        {
+            var streamReader = new StreamReader(_path);
+            return streamReader.BaseStream;
+        }
+        catch
+        {
+            return new MemoryStream();
+        }
+    }
+}
--- src/Client/Shared/Services/Implementations/FxFileProvider.cs
+++ src/Client/Shared/Services/Implementations/FxFileProvider.cs
@@ -1,128 +1,49 @@
-﻿using Microsoft.Extensions.FileProviders;
+﻿using Functionland.FxFiles.Client.Shared.Services.Implementations.FxFileInfo;
+
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Primitives;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+namespace Functionland.FxFiles.Client.Shared.Services.Implementations;
 
-namespace Functionland.FxFiles.Client.Shared.Services.Implementations
+public class FxFileProvider : IFileProvider
 {
-    public class FxFileProvider : IFileProvider
+    private readonly IFileProvider _fileProvider;
+
+    public FxFileProvider(IFileProvider fileProvider)
     {
-        private readonly IFileProvider _fileProvider;
+        _fileProvider = fileProvider;
+    }
 
-        public FxFileProvider(IFileProvider fileProvider)
-        {
-            _fileProvider = fileProvider;
-        }
+    public IDirectoryContents GetDirectoryContents(string subpath)
+    {
+        return _fileProvider.GetDirectoryContents(subpath);
+    }
 
-        public IDirectoryContents GetDirectoryContents(string subpath)
-        {
-            return _fileProvider.GetDirectoryContents(subpath);
-        }
+    public IFileInfo GetFileInfo(string subpath)
+    {
+        if (subpath.StartsWith("fula://"))
+            return new FulaFileInfo(PreparePath(subpath));
+        else if (subpath.StartsWith("storage://"))
+            return new StorageFileInfo(PreparePath(subpath));
 
-        public IFileInfo GetFileInfo(string subpath)
-        {
-            if (subpath.StartsWith("fula://"))
-                return new FulaFileInfo(PreparePath(subpath));
-            else if (subpath.StartsWith("storage://") || subpath.StartsWith("wwwroot://"))
-                return new StorageFileInfo(PreparePath(subpath));
+        return _fileProvider.GetFileInfo(subpath);
+    }
 
-            //if (subpath.EndsWith(".HandleByApp=true"))
-            //{
-            //    subpath = subpath.Replace("%20", " ");
-            //    subpath = subpath.Replace(".HandleByApp=true", string.Empty);
-            //    subpath = subpath.Replace("_content/Functionland.FxFiles.Client.Shared/", string.Empty);
+    public IChangeToken Watch(string filter)
+    {
+        return _fileProvider.Watch(filter);
+    }
 
-            //    return new FxFileInfo(subpath);
-            //}
-
-            return _fileProvider.GetFileInfo(subpath);
-        }
-
-        public IChangeToken Watch(string filter)
-        {
-            return _fileProvider.Watch(filter);
-        }
-
-        private string PreparePath(string path)
-        {
-            path = path.Replace("fula://", string.Empty);
-            path = path.Replace("storage://", string.Empty);
-            path = path.Replace("wwwroot://", string.Empty);
-            return path;
-        }
+    private string PreparePath(string path)
+    {
+        path = path.Replace("fula://", string.Empty);
+        path = path.Replace("storage://", string.Empty);
+        path = WebUtility.UrlDecode(path);
+        return path;
     }
 }
 
-public class StorageFileInfo : IFileInfo
-{
-    private readonly string _path;
 
-    public StorageFileInfo(string path)
-    {
-        _path = path;
-    }
 
-    public bool Exists => true;
 
-    public long Length => 0;
-
-    public string PhysicalPath => null;
-
-    public string Name => null;
-
-    public DateTimeOffset LastModified => DateTimeOffset.FromUnixTimeSeconds(0);
-
-    public bool IsDirectory => false;
-
-    public Stream CreateReadStream()
-    {
-        try
-        {
-            var streamReader = new StreamReader(_path);
-            return streamReader.BaseStream;
-        }
-        catch
-        {
-            return new MemoryStream();
-        }
-    }
-}
-
-public class FulaFileInfo : IFileInfo
-{
-    private readonly string _path;
-
-    public FulaFileInfo(string path)
-    {
-        _path = path;
-    }
-
-    public bool Exists => true;
-
-    public long Length => 0;
-
-    public string PhysicalPath => null;
-
-    public string Name => null;
-
-    public DateTimeOffset LastModified => DateTimeOffset.FromUnixTimeSeconds(0);
-
-    public bool IsDirectory => false;
-
-    public Stream CreateReadStream()
-    {
-        try
-        {
-            var streamReader = new StreamReader(_path);
-            return streamReader.BaseStream;
-        }
-        catch
-        {
-            return new MemoryStream();
-        }
-    }
-}
