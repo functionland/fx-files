3ff3a290f5d633f05a0e157feecbcbc311a0ced0
Zahra Ahangari <ahangari95@gmail.com>
2022-10-16 00:47:55 +0330

    Add FulaRootPath to convention. ab#709

--- src/Client/Shared/Utils/FulaConvention.cs
+++ src/Client/Shared/Utils/FulaConvention.cs
@@ -2,6 +2,7 @@
 
 public static class FulaConvention
 {
+    public const string FulaRootPath = "/";
     public const string FulaFilesRootPath = "MyFiles";
-    public const string FulaSharedRootPath = "Share";
+    public const string FulaSharedRootPath = "Shared";
 }
