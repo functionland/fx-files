9588123be43f782d146da7a14769c95351d671e6
Hootan Hemmati <hootanhemmati@outlook.com>
2022-10-15 10:01:13 +0330

    Add design check box ab#120

--- src/Client/Shared/Components/DesignSystem/FxCheckBox.razor.cs
+++ src/Client/Shared/Components/DesignSystem/FxCheckBox.razor.cs
@@ -2,5 +2,19 @@
 {
     public partial class FxCheckBox
     {
+        [Parameter]
+        public bool IsChecked { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> IsCheckedChanged { get; set; }
+
+        [Parameter]
+        public bool IsEnable { get; set; } = true;
+
+        private void OnChange()
+        {
+            IsChecked = !IsChecked;
+            IsCheckedChanged.InvokeAsync(IsChecked);
+        }
     }
 }
