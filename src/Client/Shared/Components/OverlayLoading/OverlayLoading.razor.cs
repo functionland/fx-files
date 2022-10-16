24a70133c7a737c648322e81ba913ee20d7a3331
Ali <Alimuhammadiii@outlook.com>
2022-10-16 18:42:34 +0330

    Fix loading ab#637

---
+++ src/Client/Shared/Components/OverlayLoading/OverlayLoading.razor.cs
@@ -0,0 +1,7 @@
+﻿namespace Functionland.FxFiles.Client.Shared.Components;
+
+public partial class OverlayLoading
+{
+    [Parameter] public string Color { get; set; } = "#808080";
+    [Parameter] public string ZIndex { get; set; } = "0";
+}
