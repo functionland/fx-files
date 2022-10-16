45e4fe336db140a0e94f0002c943a21901f4649d
Hamed Hajiloo <45947371+hamedhajiloo@users.noreply.github.com>
2022-10-17 01:22:46 +0330

    Change FsArtifact[] to IEnumerable<FsArtifact>. ab#730

--- src/Client/Shared/Services/Contracts/PinService/IPinService.cs
+++ src/Client/Shared/Services/Contracts/PinService/IPinService.cs
@@ -4,8 +4,8 @@ public interface IPinService
 {
     Task InitializeAsync(CancellationToken? cancellationToken = null);
     Task EnsureInitializedAsync(CancellationToken? cancellationToken = null);
-    Task SetArtifactsPinAsync(FsArtifact[] artifact, CancellationToken? cancellationToken = null);
-    Task SetArtifactsUnPinAsync(string[] path, CancellationToken? cancellationToken = null);
+    Task SetArtifactsPinAsync(IEnumerable<FsArtifact> artifact, CancellationToken? cancellationToken = null);
+    Task SetArtifactsUnPinAsync(IEnumerable<string> path, CancellationToken? cancellationToken = null);
     Task<List<FsArtifact>> GetPinnedArtifactsAsync(CancellationToken? cancellationToken = null);
     Task<bool> IsPinnedAsync(FsArtifact artifact, CancellationToken? cancellationToken = null);
 }
