829ef0edb58b2ed114e98472fddbef1dcdab689e
Mmmad Amin Mahjub Asl <aminmahjoob3172@gmail.com>
2022-10-16 13:37:39 +0330

    Implement welcome page component ab#464

---
+++ src/Client/Shared/Pages/_Onboarding/WelcomePage/WelcomePage.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functionland.FxFiles.Client.Shared.Pages._Onboarding.WelcomePage
+{
+    public partial class Welcome_page
+    {
+    }
+}
