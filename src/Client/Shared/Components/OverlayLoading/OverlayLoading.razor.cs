c716c345b6461ba921a7e42ebf03eabe7359eb96
Maryam Davoudi <davoudi.maryam@outlook.com>
2022-10-17 16:52:26 +0330

    Fix ovarlayLoading components ab#647

--- src/Client/Shared/Components/OverlayLoading/OverlayLoading.razor.cs
+++ src/Client/Shared/Components/OverlayLoading/OverlayLoading.razor.cs
@@ -1,7 +1,7 @@
-﻿namespace Functionland.FxFiles.Client.Shared.Components;
-
+﻿
+namespace Functionland.FxFiles.Client.Shared.Components;
 public partial class OverlayLoading
 {
-    [Parameter] public string Color { get; set; } = "#808080";
+    //[Parameter] public string Color { get; set; } = "#808080";
     [Parameter] public string ZIndex { get; set; } = "0";
 }
