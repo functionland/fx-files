104e3bd8a8bdf804e2b6c9227d5b3be98cadc3fd
Hamed Hajiloo <45947371+hamedhajiloo@users.noreply.github.com>
2022-10-19 23:34:54 +0330

    Create AndroidImageThumbnailPlugin. ab#827

---
+++ src/Client/App/Platforms/Android/Implementations/AndroidImageThumbnailPlugin.cs
@@ -0,0 +1,24 @@
+﻿using Android.Graphics;
+using Android.Media;
+using Stream = System.IO.Stream;
+
+namespace Functionland.FxFiles.Client.App.Platforms.Android.Implementations;
+
+public class AndroidImageThumbnailPlugin : IThumbnailPlugin
+{
+    public async Task<Stream> CreateThumbnailAsync(Stream input, CancellationToken? cancellationToken = null)
+    {
+        var bitmap = BitmapFactory.DecodeStream(input);
+        var imageThumbnail = await ThumbnailUtils.ExtractThumbnailAsync(bitmap, 252, 146);
+
+        var outputStream = new MemoryStream();
+        await imageThumbnail.CompressAsync(Bitmap.CompressFormat.Jpeg, 100, outputStream);
+
+        return outputStream;
+    }
+
+    public bool IsExtensionSupported(string extension)
+    {
+        throw new NotImplementedException();
+    }
+}
