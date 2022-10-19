c223b9c3eff3781f1861083d1a9e7e5b4d18ca79
Mmmad Amin Mahjub Asl <aminmahjoob3172@gmail.com>
2022-10-19 12:44:04 +0330

    Add back btn functionality in about page ab#765

--- src/Client/Shared/Pages/_Settings/SettingAboutPage/SettingAbout.razor.cs
+++ src/Client/Shared/Pages/_Settings/SettingAboutPage/SettingAbout.razor.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Functionland.FxFiles.Client.Shared.Pages._Settings.SettingAbout
+namespace Functionland.FxFiles.Client.Shared.Pages
 {
     public partial class SettingAbout
     {
+        private void HandleToolbarBack()
+        {
+            NavigationManager.NavigateTo("settings", false, true);
+        }
     }
 }
