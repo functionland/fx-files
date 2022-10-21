d35a3ee9c0afdd276a63b2a46a9f2352794aa400
Zahra Ahangari <ahangari95@gmail.com>
2022-10-22 03:18:58 +0330

    Add size to Android video thumbnail output. ab#907

--- src/Client/App/Platforms/Android/Implementations/AndroidVideoThumbnailPlugin.cs
+++ src/Client/App/Platforms/Android/Implementations/AndroidVideoThumbnailPlugin.cs
@@ -5,6 +5,7 @@ using Functionland.FxFiles.Client.Shared.Utils;
 using Bitmap = Android.Graphics.Bitmap;
 using ExifInterface = AndroidX.ExifInterface.Media.ExifInterface;
 using Stream = System.IO.Stream;
+using Size = Android.Util.Size;
 
 namespace Functionland.FxFiles.Client.App.Platforms.Android.Implementations;
 
@@ -25,7 +26,8 @@ public class AndroidVideoThumbnailPlugin : VideoThumbnailPlugin
             throw new InvalidOperationException("FilePath should be provided for this plugin.");
         }
 
-        var bitmap = await ThumbnailUtils.CreateVideoThumbnailAsync(new Java.IO.File(filePath), null, null);
+        var size = new Size(1280, 640);
+        var bitmap = await ThumbnailUtils.CreateVideoThumbnailAsync(new Java.IO.File(filePath), size, null);
         var outputStream = new MemoryStream();
         await bitmap.CompressAsync(Bitmap.CompressFormat.Jpeg, 100, outputStream);
 
