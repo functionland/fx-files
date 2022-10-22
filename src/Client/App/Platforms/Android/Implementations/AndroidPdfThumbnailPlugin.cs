e439b5f2da71f75da164a56a205f36a44f927eee
Hamed Hajiloo <45947371+hamedhajiloo@users.noreply.github.com>
2022-10-22 22:22:27 +0330

    Use PdfRenderer for pdf thumnail in android. ab#827

--- src/Client/App/Platforms/Android/Implementations/AndroidPdfThumbnailPlugin.cs
+++ src/Client/App/Platforms/Android/Implementations/AndroidPdfThumbnailPlugin.cs
@@ -1,61 +1,82 @@
 ﻿using Android.Graphics;
-using Android.Media;
+using Android.Graphics.Pdf;
+using Android.OS;
 using Functionland.FxFiles.Client.Shared.Enums;
-using Functionland.FxFiles.Client.Shared.Utils;
-using iTextSharp.text.pdf;
-using Java.Security;
-using Spire.Pdf;
-using Spire.Pdf.OPC;
-using System.IO;
-using System.Reflection.PortableExecutable;
-using static Android.Provider.DocumentsContract;
-using Bitmap = Android.Graphics.Bitmap;
-using PdfDocument = iTextSharp.text.pdf.PdfDocument;
 using Stream = System.IO.Stream;
 
 namespace Functionland.FxFiles.Client.App.Platforms.Android.Implementations;
 
 public class AndroidPdfThumbnailPlugin : PdfThumbnailPlugin
 {
-    public override bool IsJustFilePathSupported => false;
+    public override bool IsJustFilePathSupported => true;
     protected override async Task<Stream> OnCreateThumbnailAsync(
-        Stream? inputStream,
+        Stream? inputPdfStream,
         string? filePath,
         ThumbnailScale thumbnailScale,
         CancellationToken? cancellationToken = null)
     {
-        //if (inputStream is not null)
-        //    throw new InvalidOperationException($"Stream is not supported by this plugin.");
+        if (inputPdfStream is not null)
+            throw new InvalidOperationException($"Stream is not supported by this plugin.");
 
-        //if (filePath is null)
-        //{
-        //    throw new InvalidOperationException("FilePath should be provided for this plugin.");
-        //}
+        if (filePath is null)
+            throw new InvalidOperationException("FilePath should be provided for this plugin.");
 
-        // Get the first page of the Pdf in bytes out of the stream.
-        var inputPdf = new PdfReader(inputStream);
-        var pageContent = inputPdf.GetPageContent(1);
+        var fileDescriptor = ParcelFileDescriptor.Open(new Java.IO.File(filePath), ParcelFileMode.ReadOnly);
 
-        // Convert the first page from bytes to bitmap.
-        var firstPageStream = new MemoryStream(pageContent);
-        var firstPageInBitmapStream =  BitmapFactory.DecodeStream(firstPageStream);
+        if (fileDescriptor is null)
+            throw new InvalidOperationException("fileDescriptor can not be null.");
 
-        var firstPageInBitmapByte = await BitmapFactory.DecodeByteArrayAsync(pageContent, 0, pageContent.Length);
+        PdfRenderer renderer = new(fileDescriptor);
+        var page = renderer.OpenPage(1);
+        if (page is null)
+            throw new InvalidOperationException("Page can not be null.");
 
-        //var fileStream = new FileStream()
-        //var fileStreamBitmap = BitmapFactory.DecodeStream(fileStream);
-        //var pdfFileThumbnail = await ThumbnailUtils.ExtractThumbnailAsync(fileStreamBitmap, 1280, 640);
+        Bitmap? bmp = Bitmap.CreateBitmap(page.Width, page.Height, Bitmap.Config.Argb8888);
 
-        //var javaFile = new Java.IO.File("/storage/emulated/0/javaStream1stPage.pdf");
+        if (bmp is null)
+            throw new InvalidOperationException("Bitmap can not be null.");
 
-        // Ready to extract the thumbnail.
-        (int imageWidth, int imageHeight) = ImageUtils.ScaleImage(firstPageInBitmapStream.Width, firstPageInBitmapStream.Height, 1280, 640);
-        var pdfThumbnail = await ThumbnailUtils.ExtractThumbnailAsync(firstPageInBitmapStream, imageWidth, imageHeight);
+        //page.Render(bmp, null, null, PdfRenderMode.ForDisplay);
 
-        // Turn the output bitmap into stream to send it out.
         var outputStream = new MemoryStream();
-        await pdfThumbnail.CompressAsync(Bitmap.CompressFormat.Jpeg, 100, outputStream);
-
+        await bmp.CompressAsync(Bitmap.CompressFormat.Jpeg, 100, outputStream);
         return outputStream;
+        
+        
+        ////if (inputStream is not null)
+        ////    throw new InvalidOperationException($"Stream is not supported by this plugin.");
+
+        ////if (filePath is null)
+        ////{
+        ////    throw new InvalidOperationException("FilePath should be provided for this plugin.");
+        ////}
+
+        //// Get the first page of the Pdf in bytes out of the stream.
+        //var inputPdf = new PdfReader(inputStream);
+        //var pageContent = inputPdf.GetPageContent(1);
+
+        //// Convert the first page from bytes to bitmap.
+        //var firstPageStream = new MemoryStream(pageContent);
+        //var firstPageInBitmapStream =  BitmapFactory.DecodeStream(firstPageStream);
+
+        //var firstPageInBitmapByte = await BitmapFactory.DecodeByteArrayAsync(pageContent, 0, pageContent.Length);
+
+        ////var fileStream = new FileStream()
+        ////var fileStreamBitmap = BitmapFactory.DecodeStream(fileStream);
+        ////var pdfFileThumbnail = await ThumbnailUtils.ExtractThumbnailAsync(fileStreamBitmap, 1280, 640);
+
+        ////var javaFile = new Java.IO.File("/storage/emulated/0/javaStream1stPage.pdf");
+
+        //// Ready to extract the thumbnail.
+        //(int imageWidth, int imageHeight) = ImageUtils.ScaleImage(firstPageInBitmapStream.Width, firstPageInBitmapStream.Height, 1280, 640);
+        //var pdfThumbnail = await ThumbnailUtils.ExtractThumbnailAsync(firstPageInBitmapStream, imageWidth, imageHeight);
+
+        //// Turn the output bitmap into stream to send it out.
+        //var outputStream = new MemoryStream();
+        //await pdfThumbnail.CompressAsync(Bitmap.CompressFormat.Jpeg, 100, outputStream);
+
+        //return outputStream;
     }
+
+
 }
