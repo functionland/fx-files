a29f126b8fdbb7d3ad13d4116e392a686518f825
khanizahra <zahrakhanii@hotmail.com>
2022-10-25 18:00:21 +0330

    Implement ZipService. ab#816

---
+++ src/Client/Shared/Services/Implementations/ZipService.cs
@@ -0,0 +1,82 @@
+﻿using SharpCompress.Archives;
+using SharpCompress.Archives.Rar;
+using SharpCompress.Archives.Zip;
+using SharpCompress.Common;
+using SharpCompress.Readers;
+
+namespace Functionland.FxFiles.Client.Shared.Services.Implementations;
+
+public abstract partial class ZipService : IZipService
+{
+    public virtual Task<List<FsArtifact>> GetZippedFsArtifactsAsync(string zipFilePath, string subDirectoriesPath, CancellationToken? cancellationToken = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task ExtractZippedArtifactAsync(string zipFullPath, string destinationPath, List<string> fileNames, bool overwrite = false, CancellationToken? cancellationToken = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual async Task ExtractZipAsync(string fullPath, string destinationPath, string? password = null, bool overwrite = false, CancellationToken? cancellationToken = null)
+    {
+        var extention = Path.GetExtension(fullPath);
+
+        if(extention == ".zip")
+        {
+            try
+            {
+                using (var archive = ZipArchive.Open(fullPath, new ReaderOptions() { Password = password }))
+                {
+                    foreach (var entry in archive.Entries)
+                    {
+                        entry.WriteToDirectory(destinationPath, new ExtractionOptions()
+                        {
+                            ExtractFullPath = true,
+                            Overwrite = overwrite
+                        });
+                    }
+                }
+            }
+            catch (CryptographicException)
+            {
+                throw new Exception("The password did not match.");
+            }
+            catch (IOException) //TODO: This should be more specific
+            {
+                throw new Exception("The artifact already exists.");
+            }
+
+        }
+        else if(extention == ".rar")
+        {
+            try
+            {
+                using (var archive = RarArchive.Open(fullPath, new ReaderOptions() { Password = password }))
+                {
+                    foreach (var entry in archive.Entries)
+                    {
+                        entry.WriteToDirectory(destinationPath, new ExtractionOptions()
+                        {
+                            ExtractFullPath = true,
+                            Overwrite = overwrite
+                        });
+                    }
+                }
+            }
+            catch (CryptographicException)
+            {
+                throw new Exception("The password did not match.");
+            }
+            catch (IOException) //TODO: This should be more specific
+            {
+                throw new Exception("The artifact already exists.");
+            }
+
+        }
+        else
+        {
+            throw new Exception(""); //TODO
+        } 
+    }
+}
