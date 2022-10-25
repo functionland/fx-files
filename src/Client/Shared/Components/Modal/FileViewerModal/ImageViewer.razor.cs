29557380491c2c779917e9fad9f1344a9152585c
Zahra Bayat <zahra.bayat13799@gmail.com>
2022-10-25 19:51:29 +0330

    Add image and video viewer ab#938

---
+++ src/Client/Shared/Components/Modal/FileViewerModal/ImageViewer.razor.cs
@@ -0,0 +1,18 @@
+﻿using Functionland.FxFiles.Client.Shared.Services.Contracts.FileViewer;
+
+namespace Functionland.FxFiles.Client.Shared.Components.Modal;
+
+public partial class ImageViewer :IFileViewerComponent
+{
+    [Parameter] public IFileService FileService { get; set; } = default!;
+    [Parameter] public FsArtifact? CurrentArtifact { get; set; }
+    [Parameter] public bool Visibility { get; set; }
+
+    public bool IsSupported(FsArtifact artifact)
+    {
+        if (artifact.FileCategory == FileCategoryType.Image)
+            return true;
+
+        return false;
+    }
+}
