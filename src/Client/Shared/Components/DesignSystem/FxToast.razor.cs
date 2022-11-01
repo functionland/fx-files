3cc9e6d3dc98353eef2eb231f8f5f1ed61666dbb
Maryam Davoudi <davoudi.maryam@outlook.com>
2022-11-01 19:17:31 +0330

    Set animation for talst ab#963

--- src/Client/Shared/Components/DesignSystem/FxToast.razor.cs
+++ src/Client/Shared/Components/DesignSystem/FxToast.razor.cs
@@ -19,7 +19,7 @@ namespace Functionland.FxFiles.Client.Shared.Components
         public EventCallback<bool> IsOpenChanged { get; set; }
 
         private static event Func<string, string, FxToastType, Task> OnShow = default!;
-        private Timer _timer = new Timer(100000);
+        private Timer _timer = new Timer(1000000);
 
         protected override Task OnInitAsync()
         {
