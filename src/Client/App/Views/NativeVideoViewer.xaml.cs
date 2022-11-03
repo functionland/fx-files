01cbb858bcff09fcefe6d8f49568d87f1ec18a97
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2022-11-04 02:55:06 +0330

    Add VideoViewer custom controls ab#985

--- src/Client/App/Views/NativeVideoViewer.xaml.cs
+++ src/Client/App/Views/NativeVideoViewer.xaml.cs
@@ -1,15 +1,11 @@
-#if ANDROID
-using Android.App;
-using Android.Util;
-#endif
-
 using CommunityToolkit.Maui.MediaElement;
-using Microsoft.Maui.Devices;
 
 namespace Functionland.FxFiles.Client.App.Views;
 
 public partial class NativeVideoViewer : ContentPage
 {
+    protected IExceptionHandler ExceptionHandler { get; set; } = default!;
+
     public NativeVideoViewer(string path)
     {
         InitializeComponent();
@@ -25,7 +21,7 @@ public partial class NativeVideoViewer : ContentPage
         base.OnSizeAllocated(width, height);
     }
 
-    private async void back_Clicked(object sender, EventArgs e)
+    private async void Back(object sender, EventArgs e)
     {
         try
         {
@@ -35,16 +31,18 @@ public partial class NativeVideoViewer : ContentPage
         }
         catch (Exception ex)
         {
-
+            ExceptionHandler.Handle(ex);
         }
     }
 
     public void Minimize(object sender, EventArgs e)
     {
         header.IsVisible = false;
+        mediaControls.IsVisible = false;
+
 #if ANDROID
-        var aspectRatio = new Rational(300, 300);
-        PictureInPictureParams.Builder pictureInPictureParamsBuilder = new PictureInPictureParams.Builder();
+        var aspectRatio = new Android.Util.Rational(300, 300);
+        Android.App.PictureInPictureParams.Builder pictureInPictureParamsBuilder = new Android.App.PictureInPictureParams.Builder();
         var piparam = pictureInPictureParamsBuilder.SetAspectRatio(aspectRatio).Build();
         Platform.CurrentActivity.EnterPictureInPictureMode(pictureInPictureParamsBuilder.Build());
 #endif
@@ -53,15 +51,24 @@ public partial class NativeVideoViewer : ContentPage
     private void media_Tapped(object sender, EventArgs e)
     {
         header.IsVisible = !header.IsVisible;
+        mediaControls.IsVisible = !mediaControls.IsVisible;
     }
 
-    private void pin_Clicked(object sender, EventArgs e)
+    private void PausePlay_Clicked(object sender, EventArgs e)
     {
-
+        if (media.CurrentState == MediaElementState.Playing)
+            media.Pause();
+        else if (media.CurrentState == MediaElementState.Paused || media.CurrentState == MediaElementState.Stopped)
+            media.Play();
     }
 
-    private void overflow_Clicked(object sender, EventArgs e)
+    private void Backward_Clicked(object sender, EventArgs e)
     {
+        media.Position = TimeSpan.FromSeconds(media.Position.TotalSeconds - 15);
+    }
 
+    private void Forward_Clicked(object sender, EventArgs e)
+    {
+        media.Position = TimeSpan.FromSeconds(media.Position.TotalSeconds + 15);
     }
 }
