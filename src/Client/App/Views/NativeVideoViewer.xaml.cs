72524c7c2a261598e36b8031d966cf8c6dec8936
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2022-11-03 22:04:24 +0330

    Add FullScreen ability to MediaElement ab#985

--- src/Client/App/Views/NativeVideoViewer.xaml.cs
+++ src/Client/App/Views/NativeVideoViewer.xaml.cs
@@ -4,6 +4,7 @@ using Android.Util;
 #endif
 
 using CommunityToolkit.Maui.MediaElement;
+using Microsoft.Maui.Devices;
 
 namespace Functionland.FxFiles.Client.App.Views;
 
@@ -12,16 +13,16 @@ public partial class NativeVideoViewer : ContentPage
     public NativeVideoViewer(string path)
     {
         InitializeComponent();
-        mediaElement.Play();
-        mediaElement.Source = MediaSource.FromFile(path);
+        media.Play();
+        media.Source = MediaSource.FromFile(path);
     }
 
     private async void close_Clicked(object sender, EventArgs e)
     {
         try
         {
-            mediaElement.Stop();
-            mediaElement.Source = null;
+            media.Stop();
+            media.Source = null;
             await Navigation.PopAsync();
         }
         catch (Exception ex)
@@ -32,15 +33,24 @@ public partial class NativeVideoViewer : ContentPage
 
     public void Minimize(object sender, EventArgs e)
     {
-
-        // Hide the controls in picture-in-picture mode.
-        //     MainActivity.mainActivity.Minimize();
 #if ANDROID
         var aspectRatio = new Rational(300, 300);
         PictureInPictureParams.Builder pictureInPictureParamsBuilder = new PictureInPictureParams.Builder();
         var piparam = pictureInPictureParamsBuilder.SetAspectRatio(aspectRatio).Build();
         Platform.CurrentActivity.EnterPictureInPictureMode(pictureInPictureParamsBuilder.Build());
 #endif
+    }
 
+    protected override void OnSizeAllocated(double width, double height)
+    {
+        media.WidthRequest = width;
+        media.HeightRequest = height;
+
+        base.OnSizeAllocated(width, height);
+    }
+
+    private void media_Tapped(object sender, EventArgs e)
+    {
+        header.IsVisible = !header.IsVisible;
     }
 }
