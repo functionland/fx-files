db4d78f21e5cb0d5eae710c73d29782a018d4b7a
Hootan Hemmati <hootanhemmati@outlook.com>
2022-11-05 23:37:41 +0330

    Add breadcrumbs for show ab#999

--- src/Client/Shared/Components/DesignSystem/FxBreadcrumbs.razor.cs
+++ src/Client/Shared/Components/DesignSystem/FxBreadcrumbs.razor.cs
@@ -2,5 +2,22 @@
 {
     public partial class FxBreadcrumbs
     {
+        [Parameter] public FsArtifact? Artifact { get; set; }
+
+        private string[]? _breadCrumbsPath;
+
+        protected override Task OnAfterRenderAsync(bool firstRender)
+        {
+            LoadBreadCrumbsPath();
+            return base.OnAfterRenderAsync(firstRender);
+        }
+        private void LoadBreadCrumbsPath()
+        {
+            if (Artifact != null)
+            {
+                _breadCrumbsPath = Artifact.ShowablePath.Trim().Split("/", StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+
     }
 }
