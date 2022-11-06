40e319c02f04febe41a520129e5e4d2e1e9065fc
Mmmad Amin Mahjub Asl <aminmahjoob3172@gmail.com>
2022-11-06 12:52:39 +0330

    Remove hasHoriznotalScrollbar in backToTop.js ab#1002

--- src/Client/Shared/wwwroot/scripts/backToTop.js
+++ src/Client/Shared/wwwroot/scripts/backToTop.js
@@ -75,13 +75,13 @@ function breadCrumbStyle() {
     let breadcrumbs = document.querySelector(".fx-breadcrumbs");
     let startEllipsis = document.querySelector(".start-ellipsis");
     let endEllipsis = document.querySelector(".end-ellipsis");
-    let hasHorizontalScrollbar = breadcrumbs.scrollWidth > breadcrumbs.clientWidth;
 
     breadcrumbs.addEventListener("scroll", () => {
         let breadcrumbsScroll = breadcrumbs.scrollLeft.toFixed();
 
         if (breadcrumbsScroll != 0) {
             startEllipsis.style.display = "block";
+            endEllipsis.style.display = "block";
             endEllipsis.classList.remove("color-changer");
 
 
@@ -90,9 +90,9 @@ function breadCrumbStyle() {
             endEllipsis.classList.add("color-changer");
         }
         
-        if (hasHorizontalScrollbar && breadcrumbs.offsetWidth + breadcrumbs.scrollLeft >= breadcrumbs.scrollWidth - 1) {
+        if (breadcrumbs.offsetWidth + breadcrumbs.scrollLeft >= breadcrumbs.scrollWidth - 1) {
             endEllipsis.classList.add("color-changer");
             endEllipsis.style.display = "block";
         }
-    }) 
+    })
 }
