1375ec24852a949278285a874d846bc2c88fab07
Zahra Bayat <zahra.bayat13799@gmail.com>
2022-11-07 22:41:54 +0330

    Fix update breadcrumbs issue in select destination model ab#999

--- src/Client/Shared/Components/DesignSystem/FxBreadcrumbs.razor.cs
+++ src/Client/Shared/Components/DesignSystem/FxBreadcrumbs.razor.cs
@@ -5,31 +5,23 @@
         [Parameter] public FsArtifact? Artifact { get; set; }
         [Parameter] public IFileService? FileService { get; set; }
 
-        private string[] _breadCrumbsPath = Array.Empty<string>();
-
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             if (firstRender)
             {
                 await JSRuntime.InvokeVoidAsync("breadCrumbStyle");
             }
-            LoadBreadCrumbsPath();
             await base.OnAfterRenderAsync(firstRender);
         }
 
-        private void LoadBreadCrumbsPath()
+        private string[] GetBreadCrumbsPath(FsArtifact artifact)
         {
-            if (Artifact == null || FileService is null)
-                return;
+            if (FileService is null)
+                throw new InvalidOperationException("");//TODo: 
 
-            if (_breadCrumbsPath.Length == 0)
-            {
-                _breadCrumbsPath = FileService.GetShowablePath(Artifact.FullPath).Trim().Split("/", StringSplitOptions.RemoveEmptyEntries);
-                StateHasChanged();
-                return;
-            }
-            _breadCrumbsPath = FileService.GetShowablePath(Artifact.FullPath).Trim().Split("/", StringSplitOptions.RemoveEmptyEntries);
+            string[] _breadCrumbsPath = FileService.GetShowablePath(artifact.FullPath).Trim().Split("/", StringSplitOptions.RemoveEmptyEntries);
+
+            return _breadCrumbsPath;
         }
-
     }
 }
