533e4bf464d53cff4a2d8bf2071b95dae8c7b30b
Mehran Davoudi <mehran.davoudi@hotmail.com>
2022-11-08 12:30:13 +0330

    Apply skipCount. ab#1091

--- src/Client/Shared/Components/ArtifactExplorer/ArtifactExplorer.razor.cs
+++ src/Client/Shared/Components/ArtifactExplorer/ArtifactExplorer.razor.cs
@@ -370,8 +370,9 @@ public partial class ArtifactExplorer
         _ = Task.Run(async () =>
         {
             await Task.Delay(300, cancellationToken);
-            await LoadThumbnailsAsync(items.Skip(_overscanCount).ToList(), cancellationToken);
-            await LoadThumbnailsAsync(items.Take(_overscanCount).ToList(), cancellationToken);
+            var skipCount = Math.Min(_overscanCount, request.StartIndex);
+            await LoadThumbnailsAsync(items.Skip(skipCount).ToList(), cancellationToken);
+            await LoadThumbnailsAsync(items.Take(skipCount).ToList(), cancellationToken);
         }, cancellationToken);
 
         return new ItemsProviderResult<FsArtifact>(items: items, totalItemCount: Artifacts.Count);
