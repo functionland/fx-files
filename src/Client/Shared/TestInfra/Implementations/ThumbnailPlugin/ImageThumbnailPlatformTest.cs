79c8e171574b498a164c4fc429bfe7ecd5e33ef1
Hamed Hajiloo <45947371+hamedhajiloo@users.noreply.github.com>
2022-11-10 17:52:08 +0330

    Improve ThumbnailPlugin tests. ab#827

--- src/Client/Shared/TestInfra/Implementations/ThumbnailPlugin/ImageThumbnailPlatformTest.cs
+++ src/Client/Shared/TestInfra/Implementations/ThumbnailPlugin/ImageThumbnailPlatformTest.cs
@@ -1,5 +1,5 @@
-﻿using System.Threading;
-using System;
+﻿using Functionland.FxFiles.Client.Shared.Utils;
+using System.Diagnostics;
 
 namespace Functionland.FxFiles.Client.Shared.TestInfra.Implementations.ThumbnailPlugin;
 
@@ -20,9 +20,30 @@ public abstract class ImageThumbnailPlatformTest<TFileService> : ArtifactThumbna
         return await GetFsArtifactAsync(fullPath, "https://www.digikala.com/mag/wp-content/uploads/2020/08/DK-rebranding2.jpg", cancellationToken);
     }
 
-    protected override Task OnPluginSpecificTestAsync(string testRoot, CancellationToken? cancellationToken = null)
+    protected override async Task OnPluginSpecificTestAsync(string testRoot, CancellationToken? cancellationToken = null)
     {
-        return Task.CompletedTask;
+        var thumbnailScales = Enum.GetValues<ThumbnailScale>();
+        foreach (var thumbnailScale in thumbnailScales)
+        {
+            var image_293_kb = "https://wallpaperaccess.com/full/1356284.jpg";
+
+            var fullPath_293_kb = Path.ChangeExtension(Path.Combine(testRoot, $"image_293_kb_{thumbnailScale}"), ".jpg");
+            var fsArtifact_293_kb = await GetFsArtifactAsync(fullPath_293_kb, image_293_kb, cancellationToken);
+
+            var (initialWidth, initilaHeight) = GetArtifactWidthAndHeight(fullPath_293_kb);
+
+
+            var (expectedWidth, expectedHeight) = ImageUtils.ScaleImage(initialWidth, initilaHeight, thumbnailScale);
+
+            var sw = new Stopwatch();
+            sw.Start();
+            var image_293_kb_thumbnailPath = await ArtifactThumbnailService.GetOrCreateThumbnailAsync(fsArtifact_293_kb, thumbnailScale, cancellationToken);
+            sw.Stop();
+            var duration = sw.ElapsedMilliseconds;
+
+            Assert.IsNotNull(image_293_kb_thumbnailPath, $"Image Thumbnail {thumbnailScale} created in {duration} ms");
+            AssertThumbnailWidthAndHeight(expectedWidth, expectedHeight, image_293_kb_thumbnailPath!);
+        }
     }
 
     private async Task<FsArtifact> GetFsArtifactAsync(string fullPath, string webUrl, CancellationToken? cancellationToken = null)
@@ -30,7 +51,7 @@ public abstract class ImageThumbnailPlatformTest<TFileService> : ArtifactThumbna
         var client = new HttpClient();
         var response = await client.GetAsync(webUrl);
 
-        using var stream =await response.Content.ReadAsStreamAsync();
+        using var stream = await response.Content.ReadAsStreamAsync();
 
         var artifact = await FileService.CreateFileAsync(fullPath, stream, cancellationToken);
         return artifact;
