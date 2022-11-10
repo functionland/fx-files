a4feda51b71c23489a072a21748a8f67d2615b9f
Zahra Ahangari <ahangari95@gmail.com>
2022-11-10 15:58:02 +0330

    Add Pdf thumbnail plugin platform test. ab#978 #979
    Co-Authored-By: Hamed Hajiloo <45947371+hamedhajiloo@users.noreply.github.com>
    Co-Authored-By: Ziba Seyfaei <63063815+zibasfy@users.noreply.github.com>

---
+++ src/Client/App/Platforms/Android/Implementations/Test/AndroidPdfThumbnailPluginPlatformTest.cs
@@ -0,0 +1,26 @@
+﻿using Functionland.FxFiles.Client.Shared.TestInfra.Implementations.ThumbnailPlugin;
+
+namespace Functionland.FxFiles.Client.App.Platforms.Android.Implementations.Test;
+
+public class AndroidPdfThumbnailPluginPlatformTest<TFileService> : PdfThumbnailPlatformTest<TFileService>
+    where TFileService : IFileService
+{
+    TFileService FileService { get; set; }
+    public AndroidPdfThumbnailPluginPlatformTest(IArtifactThumbnailService<TFileService> artifactThumbnailService, TFileService fileService)
+        : base(artifactThumbnailService, fileService)
+    {
+        FileService = fileService;
+    }
+
+    public override string Title => "AndroidPdfThumbnailPluginPlatformTest";
+
+    public override string Description => "Test for creating pdf thumbnail on Android.";
+
+    protected override string OnGetRootPath() => "/storage/emulated/0/";
+
+    protected override (int width, int height) GetImageWidthAndHeight(string pdfPath)
+    {
+        //ToDo: Grab the first page of the pdf and get its dimension.
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Client/App/Platforms/Windows/Implementations/Test/WindowsPdfThumbnailPluginPlatformTest.cs
@@ -0,0 +1,26 @@
+﻿using Functionland.FxFiles.Client.Shared.TestInfra.Implementations.ThumbnailPlugin;
+
+namespace Functionland.FxFiles.Client.App.Platforms.Windows.Implementations.Test;
+
+public class WindowsPdfThumbnailPluginPlatformTest<TFileService> : PdfThumbnailPlatformTest<TFileService>
+    where TFileService : IFileService
+{
+    IFileService FileService { get; set; }
+    public WindowsPdfThumbnailPluginPlatformTest(IArtifactThumbnailService<TFileService> artifactThumbnailService, TFileService fileService)
+        : base(artifactThumbnailService, fileService)
+    {
+        FileService = fileService;
+    }
+
+    public override string Title => $"WindowsPdfThumbnailPluginPlatformTest {typeof(TFileService).Name}";
+
+    public override string Description => "Test for create pdf thumbnail on windows";
+
+    protected override string OnGetRootPath() => "C:\\";
+
+    protected override (int width, int height) GetImageWidthAndHeight(string pdfPath)
+    {
+        //ToDo: Grab only the first page of pdf file from its path.
+        throw new NotImplementedException();
+    }
+}
