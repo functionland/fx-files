a4a9ec69864e1a1b95b1a1c45fd0b972e66a0e2f
Zahra Bayat <zahra.bayat13799@gmail.com>
2022-11-14 21:16:11 +0330

    Implement File Launcher for Ios and Mac ab#1129

--- src/Client/App/Platforms/MacCatalyst/Implementations/MacFileLauncher.cs
+++ src/Client/App/Platforms/MacCatalyst/Implementations/MacFileLauncher.cs
@@ -4,4 +4,9 @@ namespace Functionland.FxFiles.Client.App.Platforms.MacCatalyst.Implementations;
 
 public class MacFileLauncher : LocalFileLauncher
 {
+    public override async Task OpenWithAsync(string filePath)
+    {
+        var uri = new Uri($"file://{filePath}");
+        await Launcher.OpenAsync(uri);
+    }
 }
--- src/Client/App/Platforms/iOS/Implementations/IosFileLauncher.cs
+++ src/Client/App/Platforms/iOS/Implementations/IosFileLauncher.cs
@@ -1,7 +1,13 @@
 ﻿using Functionland.FxFiles.Client.App.Implementations;
+using Functionland.FxFiles.Client.Shared.Models;
 
 namespace Functionland.FxFiles.Client.App.Platforms.iOS.Implementations;
 
 public class IosFileLauncher : LocalFileLauncher
 {
+    public override async Task OpenWithAsync(string filePath)
+    {
+        var uri = new Uri($"file://{filePath}");
+        await Launcher.OpenAsync(uri);
+    }
 }
