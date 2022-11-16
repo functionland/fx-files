b57883b685f7315c896f15fb611b1cf642989c95
Ziba Seyfaei <ziba.seifaie@outlook.com>
2022-11-16 10:24:19 +0330

    Fix inline search bug. ab #1101
    Co-Authored-By: Zahra Bayat <Zahra.bayat13799@gmail.com>

--- src/Client/Shared/Components/FileBrowser/FileBrowser.razor.cs
+++ src/Client/Shared/Components/FileBrowser/FileBrowser.razor.cs
@@ -896,9 +896,9 @@ public partial class FileBrowser
 
     private async Task HandleSelectArtifactAsync(FsArtifact artifact)
     {
-        _fxSearchInputRef?.HandleClearInputText();
         if (artifact.ArtifactType == FsArtifactType.File)
         {
+            _fxSearchInputRef?.HandleClearInputText();
             var isOpened = _fileViewerRef != null && await _fileViewerRef.OpenArtifact(artifact);
 
             if (isOpened == false)
@@ -943,11 +943,6 @@ public partial class FileBrowser
         }
         else
         {
-            if (_isInSearch)
-            {
-                await CancelSearchAsync();
-            }
-
             await OpenFolderAsync(artifact);
         }
     }
@@ -968,6 +963,12 @@ public partial class FileBrowser
 
             CurrentArtifact = artifact;
             _displayedArtifacts = new List<FsArtifact>();
+
+            if (!string.IsNullOrWhiteSpace(_inlineSearchText))
+            {
+                _fxSearchInputRef?.HandleClearInputText();
+                _inlineSearchText = string.Empty;
+            }
 
             _ = Task.Run(async () =>
             {
