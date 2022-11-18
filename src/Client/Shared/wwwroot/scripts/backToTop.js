1208f26295e3023534d7b6f0d36344eb4ee3bc46
Mmmad Amin Mahjub Asl <aminmahjoob3172@gmail.com>
2022-11-18 18:42:29 +0330

    Add breadcrumbs functionality in selection modal ab#1338

--- src/Client/Shared/wwwroot/scripts/backToTop.js
+++ src/Client/Shared/wwwroot/scripts/backToTop.js
@@ -110,6 +110,37 @@ function breadCrumbStyle() {
     });
 }
 
+function breadCrumbStyleSelectionModal() {
+    let breadcrumbs = document.querySelector(".sheet-wrapper .fx-breadcrumbs");
+    let startEllipsis = document.querySelector(".sheet-wrapper .start-ellipsis");
+    let endEllipsis = document.querySelector(".sheet-wrapper .end-ellipsis");
+    let hasHorizontalScrollbar = breadcrumbs.scrollWidth > breadcrumbs.clientWidth;
+
+    if (hasHorizontalScrollbar) {
+        startEllipsis.style.display = "block";
+        breadcrumbs.scrollLeft = breadcrumbs.scrollWidth + breadcrumbs.scrollLeft;
+    }
+
+    breadcrumbs.addEventListener("scroll", () => {
+        startEllipsis.style.display = "block";
+        let breadcrumbsScroll = breadcrumbs.scrollLeft.toFixed();
+
+        if (breadcrumbsScroll != 0) {
+            startEllipsis.classList.add("color-changer");
+            startEllipsis.style.display = "block";
+            endEllipsis.classList.add("color-changer");
+
+        } else {
+            startEllipsis.classList.remove("color-changer");
+            startEllipsis.style.display = "none";
+        }
+
+        if (breadcrumbs.offsetWidth + breadcrumbs.scrollLeft >= breadcrumbs.scrollWidth - 1) {
+            endEllipsis.classList.remove("color-changer");
+        }
+    });
+}
+
 
 function OnScrollCheck() {
     const artifactListDiv = document.querySelector('.list-container');
