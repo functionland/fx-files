d101b3aac49aa684c33934912159018dccf019f5
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2022-11-23 02:04:50 +0330

    Replace MCT MediaElement with Video control ab#1409

---
+++ src/Client/App/Controls/ResourceVideoSource.cs
@@ -0,0 +1,14 @@
+﻿namespace Functionland.FxFiles.Client.App.Controls
+{
+    public class ResourceVideoSource : VideoSource
+    {
+        public static readonly BindableProperty PathProperty =
+            BindableProperty.Create(nameof(Path), typeof(string), typeof(ResourceVideoSource));
+
+        public string Path
+        {
+            get { return (string)GetValue(PathProperty); }
+            set { SetValue(PathProperty, value); }
+        }
+    }
+}
