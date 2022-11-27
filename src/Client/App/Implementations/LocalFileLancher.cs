4b30a7b0c74e3f8f7f806eb8ef718f9c9dd64f4c
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2022-11-27 20:21:14 +0330

    Refactor LocalFileLauncher ab#1090

--- src/Client/App/Implementations/LocalFileLancher.cs
+++ src/Client/App/Implementations/LocalFileLancher.cs
@@ -1,22 +1,8 @@
-﻿using Functionland.FxFiles.Client.Shared.Models;
-using System.Diagnostics;
-
-using Microsoft.Maui.Controls.PlatformConfiguration;
-
-namespace Functionland.FxFiles.Client.App.Implementations;
+﻿namespace Functionland.FxFiles.Client.App.Implementations;
 
 public abstract class LocalFileLauncher : IFileLauncher
 {
-    public virtual async Task<bool> OpenWithAsync(string filePath)
-    {
-        var isOpen = await Launcher.OpenAsync(new OpenFileRequest
-        {
-            Title = "Open with",
-            File = new ReadOnlyFile(filePath)
-        });
-
-        return isOpen;
-    }
+    public abstract Task<bool> OpenWithAsync(string filePath);
 
     public abstract Task OpenFileAsync(string filePath);
 }
