6ee2b46c4b34812465d8e491634e2b0783093f91
Maryam Davoudi <davoudi.maryam@outlook.com>
2022-11-29 02:32:18 +0330

    Fix breadcrumb scroll ab#1408

--- src/Client/Shared/Components/DesignSystem/FxBreadcrumbs.razor.cs
+++ src/Client/Shared/Components/DesignSystem/FxBreadcrumbs.razor.cs
@@ -3,11 +3,13 @@
     public partial class FxBreadcrumbs
     {
         private string[] _breadcrumbs = new string[0];
+
+        [Parameter] public string Source { get; set; } = "browsing";
         [Parameter] public string[] BreadcrumbsPath { get => _breadcrumbs; 
             set {
-                if (_breadcrumbs == value) return;
+                //if (_breadcrumbs == value) return;
                 _breadcrumbs = value;
-                JSRuntime.InvokeVoidAsync("breadCrumbStyleSelectionModal");
+                JSRuntime.InvokeVoidAsync("breadCrumbStyle", Source);
             }
         }
 
--- src/Client/Shared/wwwroot/scripts/backToTop.js
+++ src/Client/Shared/wwwroot/scripts/backToTop.js
@@ -57,54 +57,59 @@ function ImagePinchZoom() {
     });
 }
 
-function breadCrumbStyle() {
-    let breadcrumbs = document.querySelector(".fx-breadcrumbs");
+function breadCrumbStyle(source) {
+    debugger
+    setTimeout(function () {
+        let breadcrumbs = document.querySelector(`.${source}-fx-breadcrumbs`);
 
-    if (!breadcrumbs)
-        return;
+        if (!breadcrumbs)
+            return;
 
-    let startEllipsis = document.querySelector(".start-ellipsis");
-    let endEllipsis = document.querySelector(".end-ellipsis");
-    let hasHorizontalScrollbar = breadcrumbs.scrollWidth > breadcrumbs.clientWidth;
+        let startEllipsis = document.querySelector(`.${source}-start-ellipsis`);
+        let endEllipsis = document.querySelector(`.${source}-end-ellipsis`);
+        let hasHorizontalScrollbar = breadcrumbs.scrollWidth > breadcrumbs.clientWidth;
 
-    if (hasHorizontalScrollbar) {
-        startEllipsis.style.display = "block";
-        breadcrumbs.scrollLeft = breadcrumbs.scrollWidth + breadcrumbs.scrollLeft;
-    }
-
-    breadcrumbs.addEventListener("scroll", () => {
-        startEllipsis.style.display = "block";
-        let breadcrumbsScroll = breadcrumbs.scrollLeft.toFixed();
-
-        if (breadcrumbsScroll != 0) {
-            startEllipsis.classList.add("color-changer");
+        if (hasHorizontalScrollbar) {
             startEllipsis.style.display = "block";
-            endEllipsis.classList.add("color-changer");
-
-        } else {
-            startEllipsis.classList.remove("color-changer");
-            startEllipsis.style.display = "none";
+            //breadcrumbs.scrollLeft = breadcrumbs.scrollWidth + breadcrumbs.scrollLeft;
+            breadcrumbs.scrollLeft = breadcrumbs.scrollWidth - breadcrumbs.clientWidth;
         }
 
-        if (breadcrumbs.offsetWidth + breadcrumbs.scrollLeft >= breadcrumbs.scrollWidth - 1) {
-            endEllipsis.classList.remove("color-changer");
-        }
-    });
+        breadcrumbs.addEventListener("scroll", () => {
+            startEllipsis.style.display = "block";
+            let breadcrumbsScroll = breadcrumbs.scrollLeft.toFixed();
+
+            if (breadcrumbsScroll != 0) {
+                startEllipsis.classList.add("color-changer");
+                startEllipsis.style.display = "block";
+                endEllipsis.classList.add("color-changer");
+
+            } else {
+                startEllipsis.classList.remove("color-changer");
+                startEllipsis.style.display = "none";
+            }
+
+            if (breadcrumbs.offsetWidth + breadcrumbs.scrollLeft >= breadcrumbs.scrollWidth - 1) {
+                endEllipsis.classList.remove("color-changer");
+            }
+        });
+    }, 100)
 }
 
-function breadCrumbStyleSelectionModal() {
-    let breadcrumbs = document.querySelector(".sheet-wrapper .fx-breadcrumbs");
+function breadCrumbStyleSelectionModal(source) {
+    let breadcrumbs = document.querySelector(`.sheet-wrapper .${source}-fx-breadcrumbs`);
 
     if (!breadcrumbs)
         return;
 
-    let startEllipsis = document.querySelector(".sheet-wrapper .start-ellipsis");
-    let endEllipsis = document.querySelector(".sheet-wrapper .end-ellipsis");
+    let startEllipsis = document.querySelector(`.sheet-wrapper .${source}-start-ellipsis`);
+    let endEllipsis = document.querySelector(`.sheet-wrapper .${source}-end-ellipsis`);
     let hasHorizontalScrollbar = breadcrumbs.scrollWidth > breadcrumbs.clientWidth;
 
     if (hasHorizontalScrollbar) {
         startEllipsis.style.display = "block";
-        breadcrumbs.scrollLeft = breadcrumbs.scrollWidth + breadcrumbs.scrollLeft;
+        //breadcrumbs.scrollLeft = breadcrumbs.scrollWidth + breadcrumbs.scrollLeft;
+        breadcrumbs.scrollLeft = breadcrumbs.scrollWidth;
     }
 
     breadcrumbs.addEventListener("scroll", () => {
