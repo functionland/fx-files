370edde36e2b2832db21d2fd0fab514fcc13c53d
Hamed Hajiloo <45947371+hamedhajiloo@users.noreply.github.com>
2022-12-01 23:51:22 +0330

    Fix svf format for android intent. ab#1602

--- src/Client/App/Platforms/Android/FileViewerActivity.cs
+++ src/Client/App/Platforms/Android/FileViewerActivity.cs
@@ -28,7 +28,7 @@ namespace Functionland.FxFiles.Client.App.Platforms.Android;
         //"video/*" 
        
         "text/plain",
-        "image/jpg","image/jpeg","image/png","image/gif","image/bmp","image/svg","image/webp","image/jfif","image/ico"
+        "image/jpg","image/jpeg","image/png","image/gif","image/bmp","image/svg+xml","image/webp","image/jfif","image/ico"
 
     })]
 public class FileViewerActivity : MainActivity
