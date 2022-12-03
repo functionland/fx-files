fe49d17d0fda2fb36c10011a857dbee58039fad8
Hamed Hajiloo <45947371+hamedhajiloo@users.noreply.github.com>
2022-12-03 17:57:47 +0330

    Fix LocalAndroidTxtLogger bug. ab#1610

--- src/Client/App/Platforms/Android/Implementations/LocalAndroidTxtLogger.cs
+++ src/Client/App/Platforms/Android/Implementations/LocalAndroidTxtLogger.cs
@@ -4,18 +4,15 @@ public static class LocalAndroidTxtLogger
 {
     public static void Log(string message)
     {
-
+#if Dev || Local
         message = $"[{DateTimeOffset.Now:yyyy/MM/dd HH:mm:ss}] --> [{message}{Environment.NewLine}]";
         var path = "/storage/emulated/0/FxFileLocalLogs.txt";
         if (!File.Exists(path))
         {
-            File.WriteAllText(path, message);
+            File.Create(path);
         }
-        else
-        {
-            var rawContent = File.ReadAllText(path);
-            File.WriteAllText(path, $"{rawContent}{Environment.NewLine}{message}");
-        }
-
+        var rawContent = File.ReadAllText(path);
+        File.WriteAllText(path, $"{rawContent}{Environment.NewLine}{message}");
+#endif
     }
 }
